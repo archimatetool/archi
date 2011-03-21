f35e22e9e7860e752291efd3599ca721678abd84
Phillipus <p.beauvoir@dadabeatnik.com>
2011-03-21 17:33:27 +0000

    Re-instate Connected GroupEditPart
    We can make this a connected EditPart with additional work on MagicConnectionCreationTool.

--- uk.ac.bolton.archimate.editor/src/uk/ac/bolton/archimate/editor/diagram/editparts/diagram/GroupEditPart.java
+++ uk.ac.bolton.archimate.editor/src/uk/ac/bolton/archimate/editor/diagram/editparts/diagram/GroupEditPart.java
@@ -8,10 +8,10 @@ package uk.ac.bolton.archimate.editor.diagram.editparts.diagram;
 
 import java.util.List;
 
+import org.eclipse.draw2d.ChopboxAnchor;
+import org.eclipse.draw2d.ConnectionAnchor;
 import org.eclipse.draw2d.IFigure;
-import org.eclipse.emf.common.notify.Adapter;
-import org.eclipse.emf.common.notify.Notification;
-import org.eclipse.emf.common.notify.impl.AdapterImpl;
+import org.eclipse.draw2d.Label;
 import org.eclipse.gef.EditPolicy;
 import org.eclipse.gef.Request;
 import org.eclipse.gef.RequestConstants;
@@ -22,20 +22,21 @@ import org.eclipse.gef.tools.DirectEditManager;
 
 import uk.ac.bolton.archimate.editor.diagram.directedit.LabelCellEditorLocator;
 import uk.ac.bolton.archimate.editor.diagram.directedit.LabelDirectEditManager;
-import uk.ac.bolton.archimate.editor.diagram.editparts.AbstractBaseEditPart;
+import uk.ac.bolton.archimate.editor.diagram.editparts.AbstractConnectedEditPart;
 import uk.ac.bolton.archimate.editor.diagram.editparts.IColoredEditPart;
 import uk.ac.bolton.archimate.editor.diagram.editparts.ITextEditPart;
 import uk.ac.bolton.archimate.editor.diagram.editparts.SnapEditPartAdapter;
 import uk.ac.bolton.archimate.editor.diagram.figures.IContainerFigure;
+import uk.ac.bolton.archimate.editor.diagram.figures.IDiagramModelObjectFigure;
 import uk.ac.bolton.archimate.editor.diagram.figures.IEditableLabelFigure;
 import uk.ac.bolton.archimate.editor.diagram.figures.diagram.GroupFigure;
 import uk.ac.bolton.archimate.editor.diagram.policies.BasicContainerEditPolicy;
 import uk.ac.bolton.archimate.editor.diagram.policies.ContainerHighlightEditPolicy;
+import uk.ac.bolton.archimate.editor.diagram.policies.DiagramConnectionPolicy;
 import uk.ac.bolton.archimate.editor.diagram.policies.DiagramLayoutPolicy;
 import uk.ac.bolton.archimate.editor.diagram.policies.GroupContainerComponentEditPolicy;
 import uk.ac.bolton.archimate.editor.diagram.policies.PartDirectEditTitlePolicy;
 import uk.ac.bolton.archimate.editor.ui.ViewManager;
-import uk.ac.bolton.archimate.model.IArchimatePackage;
 import uk.ac.bolton.archimate.model.IDiagramModelContainer;
 import uk.ac.bolton.archimate.model.IDiagramModelObject;
 
@@ -43,59 +44,24 @@ import uk.ac.bolton.archimate.model.IDiagramModelObject;
 /**
  * Group Edit Part
  * 
- * This is not a Connected EditPart because:
- * 1. A connection anchor point looks ugly if it enters at the top-right missing section of the figure
- * 2. The Magic Connector snaps to the edge of the figure rather than clicking in space
- * 
  * @author Phillip Beauvoir
  */
-public class GroupEditPart extends AbstractBaseEditPart
+public class GroupEditPart extends AbstractConnectedEditPart
 implements IColoredEditPart, ITextEditPart {
     
-    private DirectEditManager fDirectEditManager;
+    private ConnectionAnchor fAnchor;
+    private DirectEditManager fManager;
 
-    private Adapter adapter = new AdapterImpl() {
-        @Override
-        public void notifyChanged(Notification msg) {
-            switch(msg.getEventType()) {
-                // Children added or removed
-                case Notification.ADD:
-                case Notification.ADD_MANY:
-                case Notification.REMOVE:
-                case Notification.REMOVE_MANY:
-                // Move notification sent from Z-Order changes in model
-                case Notification.MOVE: 
-                    refreshChildren();
-                    break;
-
-                case Notification.SET:
-                    Object feature = msg.getFeature();
-                    if(feature == IArchimatePackage.Literals.DIAGRAM_MODEL_OBJECT__BOUNDS) {
-                        refreshBounds();
-                    }
-                    else {
-                        refreshFigure();
-                    }
-                    break;
-
-                default:
-                    break;
-            }
-        }
-    };
-    
     @Override
     protected List<?> getModelChildren() {
         return ((IDiagramModelContainer)getModel()).getChildren();
     }
 
     @Override
-    protected Adapter getECoreAdapter() {
-        return adapter;
-    }
-
-    @Override
     protected void createEditPolicies() {
+        // Allow parts to be connected
+        installEditPolicy(EditPolicy.GRAPHICAL_NODE_ROLE, new DiagramConnectionPolicy());
+
         // Add a policy to handle directly editing the Parts (for example, directly renaming a part)
         installEditPolicy(EditPolicy.DIRECT_EDIT_ROLE, new PartDirectEditTitlePolicy());
 
@@ -122,49 +88,44 @@ implements IColoredEditPart, ITextEditPart {
     }
     
     @Override
-    public IEditableLabelFigure getFigure() {
-        return (IEditableLabelFigure)super.getFigure();
-    }
-    
-    @Override
     public IFigure getContentPane() {
         return ((IContainerFigure)getFigure()).getContentPane();
     }
 
     @Override
     protected void refreshFigure() {
-        getFigure().refreshVisuals();
+        // Refresh the figure if necessary
+        ((IDiagramModelObjectFigure)getFigure()).refreshVisuals();
     }
 
+    /** 
+     * Edit Requests are handled here
+     */
     @Override
     public void performRequest(Request request) {
-        // REQ_DIRECT_EDIT is Single-click when already selected or a Rename command
-        // REQ_OPEN is Double-click
         if(request.getType() == RequestConstants.REQ_DIRECT_EDIT || request.getType() == RequestConstants.REQ_OPEN) {
-            if(request instanceof LocationRequest) {
-                // Edit the text control if we clicked on it
-                if(getFigure().didClickLabel(((LocationRequest)request).getLocation().getCopy())) {
-                    getDirectEditManager().show();
+            // Edit the label if we clicked on it
+            if(((IEditableLabelFigure)getFigure()).didClickLabel(((LocationRequest)request).getLocation().getCopy())) {
+                if(fManager == null) {
+                    Label label = ((IEditableLabelFigure)getFigure()).getLabel();
+                    fManager = new LabelDirectEditManager(this, new LabelCellEditorLocator(label), label);
                 }
-                // Else open Properties View on double-click
-                else if(request.getType() == RequestConstants.REQ_OPEN){
-                    ViewManager.showViewPart(ViewManager.PROPERTIES_VIEW, true);
-                }
+                fManager.show();
             }
-            else {
-                getDirectEditManager().show();
+            // Open Properties view
+            else if(request.getType() == RequestConstants.REQ_OPEN) {
+                ViewManager.showViewPart(ViewManager.PROPERTIES_VIEW, true);
             }
         }
     }
     
-    protected DirectEditManager getDirectEditManager() {
-        if(fDirectEditManager == null) {
-            fDirectEditManager = new LabelDirectEditManager(this, new LabelCellEditorLocator(getFigure().getLabel()),
-                    getFigure().getLabel());
+    @Override
+    protected ConnectionAnchor getConnectionAnchor() {
+        if(fAnchor == null) {
+            fAnchor = new ChopboxAnchor(getFigure());
         }
-        return fDirectEditManager;
+        return fAnchor;
     }
-
 
     @SuppressWarnings("rawtypes")
     @Override
@@ -175,5 +136,4 @@ implements IColoredEditPart, ITextEditPart {
         
         return super.getAdapter(adapter);
     }
-
 }
