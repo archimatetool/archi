0556f61280fcae41a6cefbc3c549ea4c561d6dba
Phillipus <p.beauvoir@dadabeatnik.com>
2011-05-05 17:01:58 +0100

    Add "Name" field to Property Section for Sketch Sticky

---
+++ uk.ac.bolton.archimate.editor/src/uk/ac/bolton/archimate/editor/propertysections/SketchStickySection.java
@@ -0,0 +1,86 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Bolton University, UK.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the License
+ * which accompanies this distribution in the file LICENSE.txt
+ *******************************************************************************/
+package uk.ac.bolton.archimate.editor.propertysections;
+
+import org.eclipse.emf.common.notify.Adapter;
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.notify.impl.AdapterImpl;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.gef.EditPart;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.PlatformUI;
+
+import uk.ac.bolton.archimate.model.IArchimatePackage;
+
+
+/**
+ * Property Section for a Sketch Sticky
+ * 
+ * @author Phillip Beauvoir
+ */
+public class SketchStickySection extends AbstractArchimatePropertySection {
+    
+    private static final String HELP_ID = "uk.ac.bolton.archimate.help.elementPropertySection";
+
+    /*
+     * Adapter to listen to changes made elsewhere (including Undo/Redo commands)
+     */
+    private Adapter eAdapter = new AdapterImpl() {
+        @Override
+        public void notifyChanged(Notification msg) {
+            Object feature = msg.getFeature();
+            // Model Name event (Undo/Redo and here)
+            if(feature == IArchimatePackage.Literals.NAMEABLE__NAME) {
+                refreshNameField();
+                fPage.labelProviderChanged(null); // Update Main label
+            }
+        }
+    };
+    
+    private EObject fEObject;
+    
+    private PropertySectionTextControl fTextName;
+    
+    @Override
+    protected void createControls(Composite parent) {
+        fTextName = createNameControl(parent, "Add a name for this sticky here");
+
+        // Help
+        PlatformUI.getWorkbench().getHelpSystem().setHelp(parent, HELP_ID);
+    }
+    
+    @Override
+    protected void setElement(Object element) {
+        if(element instanceof EditPart && ((EditPart)element).getModel() instanceof EObject) {
+            fEObject = (EObject)((EditPart)element).getModel();
+        }
+
+        if(fEObject == null) {
+            throw new RuntimeException("Object was null");
+        }
+    }
+    
+    @Override
+    public void refresh() {
+        // Populate fields...
+        refreshNameField();
+    }
+    
+    protected void refreshNameField() {
+        fTextName.refresh(fEObject);
+    }
+    
+    @Override
+    protected Adapter getECoreAdapter() {
+        return eAdapter;
+    }
+
+    @Override
+    protected EObject getEObject() {
+        return fEObject;
+    }
+}
