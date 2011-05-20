689171ede7aa74a7a3a82e240659d2b4719f1c7e
Phillipus <p.beauvoir@dadabeatnik.com>
2011-05-21 00:30:58 +0100

    DiagramModelReferenceFigure gets icon from ImageFactory
    Get the icon image from ImageFactory

--- uk.ac.bolton.archimate.editor/src/uk/ac/bolton/archimate/editor/diagram/figures/diagram/DiagramModelReferenceFigure.java
+++ uk.ac.bolton.archimate.editor/src/uk/ac/bolton/archimate/editor/diagram/figures/diagram/DiagramModelReferenceFigure.java
@@ -11,8 +11,7 @@ import org.eclipse.swt.graphics.Image;
 import uk.ac.bolton.archimate.editor.diagram.figures.AbstractTextFlowFigure;
 import uk.ac.bolton.archimate.editor.diagram.figures.RectangleFigureDelegate;
 import uk.ac.bolton.archimate.editor.diagram.figures.ToolTipFigure;
-import uk.ac.bolton.archimate.editor.ui.IArchimateImages;
-import uk.ac.bolton.archimate.model.IArchimatePackage;
+import uk.ac.bolton.archimate.editor.ui.ImageFactory;
 import uk.ac.bolton.archimate.model.IDiagramModelObject;
 import uk.ac.bolton.archimate.model.IDiagramModelReference;
 
@@ -31,16 +30,7 @@ extends AbstractTextFlowFigure {
         RectangleFigureDelegate figureDelegate = new RectangleFigureDelegate(this) {
             @Override
             public Image getImage() {
-                switch(((IDiagramModelReference)getDiagramModelObject()).getReferencedModel().eClass().getClassifierID()) {
-                    case IArchimatePackage.DIAGRAM_MODEL:
-                        return IArchimateImages.ImageFactory.getImage(IArchimateImages.ICON_DIAGRAM_16);
-                        
-                    case IArchimatePackage.SKETCH_MODEL:
-                        return IArchimateImages.ImageFactory.getImage(IArchimateImages.ICON_SKETCH_16);
-
-                    default:
-                        return IArchimateImages.ImageFactory.getImage(IArchimateImages.ICON_DIAGRAM_16);
-                }
+                return ImageFactory.getImage(((IDiagramModelReference)getDiagramModelObject()).getReferencedModel().eClass());
             }
         };
         
