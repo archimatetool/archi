c5ac8353feb92eb8e18c513b0f197b721875cbfa
Phillipus <p.beauvoir@dadabeatnik.com>
2011-08-03 11:40:30 +0100

    Move Full Screen Mac OS X 10.7 support to plug-in fragment

---
+++ uk.ac.bolton.archimate.editor.fullscreen.macos/src/uk/ac/bolton/archimate/editor/Startup.java
@@ -0,0 +1,73 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Bolton University, UK.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the License
+ * which accompanies this distribution in the file LICENSE.txt
+ *******************************************************************************/
+package uk.ac.bolton.archimate.editor;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IStartup;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+
+/**
+ * Show Full Screen widget on Mac OS X 10.7 and above if available
+ * 
+ * @author Phillip Beauvoir
+ */
+public class Startup implements IStartup {
+
+    @Override
+    public void earlyStartup() {
+        final IWorkbench workbench = PlatformUI.getWorkbench();
+        workbench.getDisplay().asyncExec(new Runnable() {
+            public void run() {
+                IWorkbenchWindow[] windows = PlatformUI.getWorkbench().getWorkbenchWindows();
+                for(IWorkbenchWindow window : windows) {
+                    showMacFullScreenWidget(window.getShell());
+                }
+            }
+        });
+    }
+
+    private void showMacFullScreenWidget(Shell shell) {
+        if(System.getProperty("os.version").startsWith("10.6")) {
+            return;
+        }
+        
+        try {
+            // Show the full-screen widget. The equivalent of:
+            // NSWindow nswindow = shell.view.window();
+            // nswindow.setCollectionBehavior(1 << 7);
+            
+            Field fieldView = Control.class.getDeclaredField("view");
+            Object nsView = fieldView.get(shell);
+            
+            Method methodWindow = fieldView.getType().getDeclaredMethod("window", new Class[] {});
+            Object nsWindow = methodWindow.invoke(nsView, new Object[] {});
+            
+            // 32-bit OS X uses int as parameter, 64-bit uses long
+            boolean is64bit = System.getProperty("os.arch").equals(Platform.ARCH_X86_64);
+            Method methodSetCollectionBehavior = nsWindow.getClass().getDeclaredMethod("setCollectionBehavior", is64bit ? long.class : int.class);
+            methodSetCollectionBehavior.invoke(nsWindow, 1 << 7);
+            
+            // Get rid of the dummy toolbar created in CocoaUIEnhancer. The equivalent of:
+            // NSWindow nswindow = shell.view.window();
+            // nswindow.setToolbar(null);
+            
+            Class<?> classNSToolbar = Class.forName("org.eclipse.swt.internal.cocoa.NSToolbar");
+            Method methodSetToolbar = nsWindow.getClass().getDeclaredMethod("setToolbar", classNSToolbar);
+            methodSetToolbar.invoke(nsWindow, new Object[] { null });
+        }
+        catch(Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
--- uk.ac.bolton.archimate.editor/src/uk/ac/bolton/archimate/editor/ArchimateEditorWorkbenchWindowAdvisor.java
+++ uk.ac.bolton.archimate.editor/src/uk/ac/bolton/archimate/editor/ArchimateEditorWorkbenchWindowAdvisor.java
@@ -6,20 +6,12 @@
  *******************************************************************************/
 package uk.ac.bolton.archimate.editor;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-
-import org.eclipse.core.runtime.Platform;
-import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.application.ActionBarAdvisor;
 import org.eclipse.ui.application.IActionBarConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
 
 import uk.ac.bolton.archimate.editor.actions.ArchimateEditorActionBarAdvisor;
-import uk.ac.bolton.archimate.editor.utils.PlatformUtils;
 
 /**
  * Workbench Window Advisor
@@ -51,42 +43,6 @@ extends WorkbenchWindowAdvisor {
         final IPlatformLauncher launcher = ArchimateEditorPlugin.INSTANCE.getPlatformLauncher();
         if(launcher != null) {
             launcher.postWindowOpen(getWindowConfigurer().getWindow());
-        }
-        
-        showMacFullScreenWidget();
-    }
-    
-    /**
-     * Show Full Screen widget on Mac OS X 10.7 and above if available
-     */
-    private void showMacFullScreenWidget() {
-        if(!PlatformUtils.isMac()) {
-            return;
-        }
-        
-        if(System.getProperty("os.version").startsWith("10.6")) {
-            return;
-        }
-        
-        // It translates to this...
-        // PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell().view.window().setCollectionBehavior(1 << 7);
-
-        try {
-            Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
-            
-            Field fieldView = Control.class.getDeclaredField("view");
-            Object nsView = fieldView.get(shell);
-            
-            Method methodWindow = fieldView.getType().getDeclaredMethod("window", new Class[] {});
-            Object nsWindow = methodWindow.invoke(nsView, new Object[] {});
-            
-            // 32-bit OS X uses int as parameter, 64-bit uses long
-            boolean is64bit = System.getProperty("os.arch").equals(Platform.ARCH_X86_64);
-            Method methodSetCollectionBehavior = nsWindow.getClass().getDeclaredMethod("setCollectionBehavior", is64bit ? long.class : int.class);
-            methodSetCollectionBehavior.invoke(nsWindow, 1 << 7);
-        }
-        catch(Exception ex) {
-            ex.printStackTrace();
         }
     }
 }
