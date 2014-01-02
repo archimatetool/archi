6e8e0925d70927479bd6b77e3c5bc6c473901d7a
Phillipus <p.beauvoir@dadabeatnik.com>
2014-01-02 14:30:22 +0000

    [Rename Package] CSV

--- com.archimatetool.csv/src/com/archimatetool/csv/Messages.java
+++ com.archimatetool.csv/src/com/archimatetool/csv/Messages.java
@@ -3,13 +3,13 @@
  * are made available under the terms of the License
  * which accompanies this distribution in the file LICENSE.txt
  */
-package uk.ac.bolton.archimate.csv;
+package com.archimatetool.csv;
 
 import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
 
-    private static final String BUNDLE_NAME = "uk.ac.bolton.archimate.csv.messages"; //$NON-NLS-1$
+    private static final String BUNDLE_NAME = "com.archimatetool.csv.messages"; //$NON-NLS-1$
 
     public static String CSVExporter_0;
 
