aba803d518ed26258ff472fd4ff9a43a11444234
Phillipus <p.beauvoir@dadabeatnik.com>
2014-02-18 10:40:27 +0000

    [Unit Tests] Add AbstractContainerFigureTests

--- tests/com.archimatetool.editor.tests/src/com/archimatetool/editor/diagram/figures/AbstractContainerFigureTests.java
+++ tests/com.archimatetool.editor.tests/src/com/archimatetool/editor/diagram/figures/AbstractContainerFigureTests.java
@@ -5,9 +5,15 @@
  */
 package com.archimatetool.editor.diagram.figures;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import org.eclipse.draw2d.Figure;
+import org.eclipse.draw2d.IFigure;
+import org.eclipse.draw2d.geometry.Dimension;
+import org.eclipse.draw2d.geometry.Point;
+import org.eclipse.draw2d.geometry.Rectangle;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -23,18 +29,22 @@ public abstract class AbstractContainerFigureTests extends AbstractDiagramModelO
 
     
     @Test
+    public void testGetMainFigure() {
+        assertNotNull(containerFigure.getMainFigure());
+    }
+    
+    @Test
     public void testGetContentPane() {
         assertNotNull(containerFigure.getContentPane());
     }
     
     @Test
     public void testTranslateMousePointToRelative() {
-        // @TODO
-    }
-    
-    @Test
-    public void testGetPreferredSize() {
-        // @TODO
+        containerFigure.setLocation(new Point(100, 100));
+        
+        Point pt = new Point(300, 400);
+        containerFigure.translateMousePointToRelative(pt);
+        assertEquals(new Point(200, 300), pt);
     }
     
     @Test
@@ -42,4 +52,30 @@ public abstract class AbstractContainerFigureTests extends AbstractDiagramModelO
         assertTrue(containerFigure.useLocalCoordinates());
     }
 
+    @Test
+    public void testGetPreferredSize() {
+        Dimension d = containerFigure.getPreferredSize();
+        assertEquals(containerFigure.getDefaultSize(), d);
+        
+        IFigure childFigure1 = new Figure();
+        childFigure1.setBounds(new Rectangle(0, 0, 200, 200));
+        containerFigure.getContentPane().add(childFigure1);
+        
+        d = containerFigure.getPreferredSize();
+        assertEquals(new Dimension(210, 210), d);
+
+        IFigure childFigure2 = new Figure();
+        childFigure2.setBounds(new Rectangle(-12, -12, 300, 256));
+        containerFigure.getContentPane().add(childFigure2);
+
+        d = containerFigure.getPreferredSize();
+        assertEquals(new Dimension(298, 254), d);
+
+        IFigure childFigure3 = new Figure();
+        childFigure3.setBounds(new Rectangle(20, 20, 300, 256));
+        containerFigure.getContentPane().add(childFigure3);
+
+        d = containerFigure.getPreferredSize();
+        assertEquals(new Dimension(330, 286), d);
+    }
 }
