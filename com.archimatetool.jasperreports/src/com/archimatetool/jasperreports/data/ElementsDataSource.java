1b19c60e2f4c9f0bf40604eea6324b54de67deb5
Phillipus <p.beauvoir@dadabeatnik.com>
2016-10-05 14:49:29 +0100

    [Archi4] Jasper Reports - add new concepts

--- com.archimatetool.jasperreports/src/com/archimatetool/jasperreports/data/ElementsDataSource.java
+++ com.archimatetool.jasperreports/src/com/archimatetool/jasperreports/data/ElementsDataSource.java
@@ -17,6 +17,7 @@ import com.archimatetool.model.IArchimateConcept;
 import com.archimatetool.model.IArchimateModel;
 import com.archimatetool.model.IArchimatePackage;
 import com.archimatetool.model.IFolder;
+import com.archimatetool.model.util.ArchimateModelUtils;
 
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JRField;
@@ -31,21 +32,13 @@ import net.sf.jasperreports.engine.JRRewindableDataSource;
  */
 public class ElementsDataSource implements JRRewindableDataSource, IPropertiesDataSource {
     
-    public static final String ELEMENTS_BUSINESS_ACTORS = "business_actors"; //$NON-NLS-1$
-    public static final String ELEMENTS_BUSINESS_FUNCTIONS = "business_functions"; //$NON-NLS-1$
-    public static final String ELEMENTS_BUSINESS_INFORMATION = "business_information"; //$NON-NLS-1$
-    public static final String ELEMENTS_BUSINESS_PROCESSES = "business_processes"; //$NON-NLS-1$
-    public static final String ELEMENTS_BUSINESS_PRODUCTS = "business_products"; //$NON-NLS-1$
-    
-    public static final String ELEMENTS_APPLICATIONS = "applications"; //$NON-NLS-1$
-    public static final String ELEMENTS_APPLICATION_DATA = "application_data"; //$NON-NLS-1$
-
-    public static final String ELEMENTS_INFRASTRUCTURES = "infrastructures"; //$NON-NLS-1$
-
+    public static final String ELEMENTS_STRATEGY = "strategy"; //$NON-NLS-1$
+    public static final String ELEMENTS_BUSINESS = "business"; //$NON-NLS-1$
+    public static final String ELEMENTS_APPLICATION = "application"; //$NON-NLS-1$
+    public static final String ELEMENTS_TECHNOLOGY = "technology"; //$NON-NLS-1$
     public static final String ELEMENTS_MOTIVATION = "motivation"; //$NON-NLS-1$
     public static final String ELEMENTS_IMPLEMENTATION_MIGRATION = "impl_migration"; //$NON-NLS-1$
-    
-    public static final String ELEMENTS_CONNECTORS = "connectors"; //$NON-NLS-1$
+    public static final String ELEMENTS_OTHER = "other"; //$NON-NLS-1$
     public static final String ELEMENTS_RELATIONS = "relations"; //$NON-NLS-1$
     
     List<IArchimateConcept> fConcepts = new ArrayList<IArchimateConcept>();
@@ -53,6 +46,7 @@ public class ElementsDataSource implements JRRewindableDataSource, IPropertiesDa
     private int currentIndex = -1;
 
     public ElementsDataSource(IArchimateModel model, String type) {
+        IFolder strategyFolder = model.getFolder(FolderType.STRATEGY);
         IFolder businessFolder = model.getFolder(FolderType.BUSINESS);
         IFolder applicationFolder = model.getFolder(FolderType.APPLICATION);
         IFolder technologyFolder = model.getFolder(FolderType.TECHNOLOGY);
@@ -60,76 +54,59 @@ public class ElementsDataSource implements JRRewindableDataSource, IPropertiesDa
         IFolder implmigrationFolder = model.getFolder(FolderType.IMPLEMENTATION_MIGRATION);
         IFolder otherFolder = model.getFolder(FolderType.OTHER);
         IFolder relationsFolder = model.getFolder(FolderType.RELATIONS);
+        
+        if(ELEMENTS_STRATEGY.equals(type)) {
+            for(EClass eClass : ArchimateModelUtils.getStrategyClasses()) {
+                getConcepts(strategyFolder, eClass);
+            }
+        }
 
-        if(ELEMENTS_BUSINESS_ACTORS.equals(type)) { 
-            getConcepts(businessFolder, IArchimatePackage.eINSTANCE.getBusinessActor());
-            getConcepts(businessFolder, IArchimatePackage.eINSTANCE.getBusinessRole());
-            getConcepts(businessFolder, IArchimatePackage.eINSTANCE.getBusinessInterface());
-            getConcepts(businessFolder, IArchimatePackage.eINSTANCE.getBusinessCollaboration());
-            getConcepts(businessFolder, IArchimatePackage.eINSTANCE.getLocation());
+        else if(ELEMENTS_BUSINESS.equals(type)) {
+            for(EClass eClass : ArchimateModelUtils.getBusinessClasses()) {
+                getConcepts(businessFolder, eClass);
+            }
         }
-        else if(ELEMENTS_BUSINESS_FUNCTIONS.equals(type)) {
-            getConcepts(businessFolder, IArchimatePackage.eINSTANCE.getBusinessFunction());
+        
+        else if(ELEMENTS_APPLICATION.equals(type)) {
+            for(EClass eClass : ArchimateModelUtils.getApplicationClasses()) {
+                getConcepts(applicationFolder, eClass);
+            }
         }
-        else if(ELEMENTS_BUSINESS_INFORMATION.equals(type)) {
-            getConcepts(businessFolder, IArchimatePackage.eINSTANCE.getBusinessObject());
-            getConcepts(businessFolder, IArchimatePackage.eINSTANCE.getRepresentation());
-            getConcepts(businessFolder, IArchimatePackage.eINSTANCE.getMeaning());
+        
+        else if(ELEMENTS_TECHNOLOGY.equals(type)) {
+            for(EClass eClass : ArchimateModelUtils.getTechnologyClasses()) {
+                getConcepts(technologyFolder, eClass);
+            }
+            for(EClass eClass : ArchimateModelUtils.getPhysicalClasses()) {
+                getConcepts(technologyFolder, eClass);
+            }
         }
-        else if(ELEMENTS_BUSINESS_PROCESSES.equals(type)) {
-            getConcepts(businessFolder, IArchimatePackage.eINSTANCE.getBusinessEvent());
-            getConcepts(businessFolder, IArchimatePackage.eINSTANCE.getBusinessInteraction());
-            getConcepts(businessFolder, IArchimatePackage.eINSTANCE.getBusinessProcess());
-        }
-        else if(ELEMENTS_BUSINESS_PRODUCTS.equals(type)) {
-            getConcepts(businessFolder, IArchimatePackage.eINSTANCE.getContract());
-            getConcepts(businessFolder, IArchimatePackage.eINSTANCE.getProduct());
-            getConcepts(businessFolder, IArchimatePackage.eINSTANCE.getBusinessService());
-            getConcepts(businessFolder, IArchimatePackage.eINSTANCE.getValue());
-        }
-        else if(ELEMENTS_APPLICATIONS.equals(type)) {
-            getConcepts(applicationFolder, IArchimatePackage.eINSTANCE.getApplicationCollaboration());
-            getConcepts(applicationFolder, IArchimatePackage.eINSTANCE.getApplicationComponent());
-            getConcepts(applicationFolder, IArchimatePackage.eINSTANCE.getApplicationFunction());
-            getConcepts(applicationFolder, IArchimatePackage.eINSTANCE.getApplicationInteraction());
-            getConcepts(applicationFolder, IArchimatePackage.eINSTANCE.getApplicationInterface());
-            getConcepts(applicationFolder, IArchimatePackage.eINSTANCE.getApplicationService());
-        }
-        else if(ELEMENTS_APPLICATION_DATA.equals(type)) {
-            getConcepts(applicationFolder, IArchimatePackage.eINSTANCE.getDataObject());
-        }
-        else if(ELEMENTS_INFRASTRUCTURES.equals(type)) {
-            getConcepts(technologyFolder, IArchimatePackage.eINSTANCE.getArtifact());
-            getConcepts(technologyFolder, IArchimatePackage.eINSTANCE.getPath());
-            getConcepts(technologyFolder, IArchimatePackage.eINSTANCE.getDevice());
-            getConcepts(technologyFolder, IArchimatePackage.eINSTANCE.getNode());
-            getConcepts(technologyFolder, IArchimatePackage.eINSTANCE.getTechnologyFunction());
-            getConcepts(technologyFolder, IArchimatePackage.eINSTANCE.getTechnologyInterface());
-            getConcepts(technologyFolder, IArchimatePackage.eINSTANCE.getCommunicationNetwork());
-            getConcepts(technologyFolder, IArchimatePackage.eINSTANCE.getTechnologyService());
-            getConcepts(technologyFolder, IArchimatePackage.eINSTANCE.getSystemSoftware());
-        }
+        
         else if(ELEMENTS_MOTIVATION.equals(type)) {
-            getConcepts(motivationFolder, IArchimatePackage.eINSTANCE.getStakeholder());
-            getConcepts(motivationFolder, IArchimatePackage.eINSTANCE.getDriver());
-            getConcepts(motivationFolder, IArchimatePackage.eINSTANCE.getAssessment());
-            getConcepts(motivationFolder, IArchimatePackage.eINSTANCE.getGoal());
-            getConcepts(motivationFolder, IArchimatePackage.eINSTANCE.getPrinciple());
-            getConcepts(motivationFolder, IArchimatePackage.eINSTANCE.getRequirement());
-            getConcepts(motivationFolder, IArchimatePackage.eINSTANCE.getConstraint());
+            for(EClass eClass : ArchimateModelUtils.getMotivationClasses()) {
+                getConcepts(motivationFolder, eClass);
+            }
         }
+        
         else if(ELEMENTS_IMPLEMENTATION_MIGRATION.equals(type)) {
-            getConcepts(implmigrationFolder, IArchimatePackage.eINSTANCE.getWorkPackage());
-            getConcepts(implmigrationFolder, IArchimatePackage.eINSTANCE.getDeliverable());
-            getConcepts(implmigrationFolder, IArchimatePackage.eINSTANCE.getPlateau());
-            getConcepts(implmigrationFolder, IArchimatePackage.eINSTANCE.getGap());
+            for(EClass eClass : ArchimateModelUtils.getImplementationMigrationClasses()) {
+                getConcepts(implmigrationFolder, eClass);
+            }
         }
-        else if(ELEMENTS_CONNECTORS.equals(type)) {
-            getConcepts(otherFolder, null);
+        
+        else if(ELEMENTS_OTHER.equals(type)) {
+            for(EClass eClass : ArchimateModelUtils.getOtherClasses()) {
+                getConcepts(otherFolder, eClass);
+            }
+            for(EClass eClass : ArchimateModelUtils.getConnectorClasses()) {
+                getConcepts(otherFolder, eClass);
+            }
         }
+        
         else if(ELEMENTS_RELATIONS.equals(type)) {
             getConcepts(relationsFolder, null);
         }
+        
         // A particular Element type in any folder
         else if(type != null) {
             EClassifier classifier =  IArchimatePackage.eINSTANCE.getEClassifier(type);
