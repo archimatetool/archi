b962344ce7b586d8c3ea5967bc420170016bec0c
tschlechter <antoine.schlechter@abac.lu>
2016-11-02 09:25:58 +0100

    Make images in html-export clickable. Avoid side effects by creating copy of model before adding additional coordinate-offset information.

--- com.archimatetool.editor/src/com/archimatetool/editor/diagram/util/DiagramUtils.java
+++ com.archimatetool.editor/src/com/archimatetool/editor/diagram/util/DiagramUtils.java
@@ -92,11 +92,14 @@ public final class DiagramUtils {
      *         If model has no children a blank image of 100x100 is returned
      */
     public static Image createImage(IDiagramModel model, double scale, int margin) {
+        return createModelReferencedImage(model, scale, margin).getImage();
+    }
+    public static ModelReferencedImage createModelReferencedImage(IDiagramModel model, double scale, int margin) {
         Shell shell = new Shell();
         shell.setLayout(new FillLayout());
         
         GraphicalViewer viewer = createViewer(model, shell);
-        Image image = createImage(viewer, scale, margin);
+        ModelReferencedImage image = createModelReferencedImage(viewer, scale, margin);
         shell.dispose();
         
         return image;
@@ -111,9 +114,12 @@ public final class DiagramUtils {
      *         If graphicalViewer has no children a blank image of 100x100 is returned
      */
     public static Image createImage(GraphicalViewer graphicalViewer, double scale, int margin) {
+        return createModelReferencedImage(graphicalViewer, scale, margin).getImage();
+    }
+    private static ModelReferencedImage createModelReferencedImage(GraphicalViewer graphicalViewer, double scale, int margin) {
         LayerManager layerManager = (LayerManager)graphicalViewer.getEditPartRegistry().get(LayerManager.ID);
         IFigure rootFigure = layerManager.getLayer(LayerConstants.PRINTABLE_LAYERS);
-        return createImage(rootFigure, scale, margin);
+        return createModelReferencedImage(rootFigure, scale, margin);
     }
     
     /**
@@ -125,6 +131,10 @@ public final class DiagramUtils {
      *         If figure has no children a blank image of 100x100 is returned
      */
     public static Image createImage(IFigure figure, double scale, int margin) {
+        return createModelReferencedImage(figure, scale, margin).getImage();
+    }
+
+    private static ModelReferencedImage createModelReferencedImage(IFigure figure, double scale, int margin) {
         if(scale <= 0) {
             scale = 1;
         }
@@ -164,7 +174,7 @@ public final class DiagramUtils {
             swtGraphics.dispose();
         }
         
-        return image;
+        return new ModelReferencedImage(image, rectangle);
     }
     
     /**
