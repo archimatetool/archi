3d27ce622c2dddd5b58dcf94057a38b9832e3e87
Phillipus <p.beauvoir@dadabeatnik.com>
2017-01-29 12:00:57 +0000

    [Validator] Don't warn about nested Junctions and Specialization relationship
    * Nested Junctions are OK
    * Specialization relationship is other way around

--- com.archimatetool.hammer/src/com/archimatetool/hammer/validation/checkers/NestedElementsChecker.java
+++ com.archimatetool.hammer/src/com/archimatetool/hammer/validation/checkers/NestedElementsChecker.java
@@ -17,11 +17,13 @@ import com.archimatetool.hammer.validation.issues.IIssue;
 import com.archimatetool.model.IAccessRelationship;
 import com.archimatetool.model.IAggregationRelationship;
 import com.archimatetool.model.IArchimateDiagramModel;
+import com.archimatetool.model.IArchimateElement;
 import com.archimatetool.model.IArchimateRelationship;
 import com.archimatetool.model.IAssignmentRelationship;
 import com.archimatetool.model.ICompositionRelationship;
 import com.archimatetool.model.IDiagramModelArchimateObject;
 import com.archimatetool.model.IDiagramModelObject;
+import com.archimatetool.model.IJunction;
 import com.archimatetool.model.IRealizationRelationship;
 import com.archimatetool.model.ISpecializationRelationship;
 
@@ -63,7 +65,7 @@ public class NestedElementsChecker implements IChecker {
                         if(dmoChild instanceof IDiagramModelArchimateObject) {
                             IDiagramModelArchimateObject child = (IDiagramModelArchimateObject)dmoChild;
                             
-                            if(!hasValidNestedRelation(parent, child)) {
+                            if(isNestedWithoutValidRelation(parent, child)) {
                                 String description =  NLS.bind(fDescription, new Object[] {
                                         child.getName(),
                                         parent.getName()
@@ -81,19 +83,36 @@ public class NestedElementsChecker implements IChecker {
         return issues;
     }
 
-    boolean hasValidNestedRelation(IDiagramModelArchimateObject parent, IDiagramModelArchimateObject child) {
-        for(IArchimateRelationship relation : parent.getArchimateElement().getSourceRelationships()) {
-            
-            if(relation.getTarget() == child.getArchimateElement()) {
+    boolean isNestedWithoutValidRelation(IDiagramModelArchimateObject parent, IDiagramModelArchimateObject child) {
+        IArchimateElement parentElement = parent.getArchimateElement();
+        IArchimateElement childElement = child.getArchimateElement();
+        
+        // Ignore nested Junctions
+        if(childElement instanceof IJunction) {
+            return false;
+        }
+        
+        // Specialization relationship goes the other way around
+        for(IArchimateRelationship relation : parentElement.getTargetRelationships()) {
+            if(relation.getSource() == childElement) {
+                if(relation instanceof ISpecializationRelationship) {
+                    return false;
+                }
+            }
+        }
+        
+        // Other relationships
+        for(IArchimateRelationship relation : parentElement.getSourceRelationships()) {
+            if(relation.getTarget() == childElement) {
                 if(relation instanceof ICompositionRelationship || relation instanceof IAggregationRelationship
-                        || relation instanceof IAssignmentRelationship || relation instanceof ISpecializationRelationship
-                        || relation instanceof IRealizationRelationship || relation instanceof IAccessRelationship) {
-                    return true;
+                        || relation instanceof IAssignmentRelationship || relation instanceof IRealizationRelationship
+                        || relation instanceof IAccessRelationship) {
+                    return false;
                 }
             }
             
         }
         
-        return false;
+        return true;
     }
 }
