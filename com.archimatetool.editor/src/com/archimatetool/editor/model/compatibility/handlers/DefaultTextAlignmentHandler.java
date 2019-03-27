b17aa1864d2d0d30774add112cdecf4b7c336310
Phillipus <p.beauvoir@dadabeatnik.com>
2019-03-27 18:33:17 +0000

    Add compatibility handler to change default Group text alignment
    * Convert centre to left

--- com.archimatetool.editor/src/com/archimatetool/editor/model/compatibility/handlers/DefaultTextAlignmentHandler.java
+++ com.archimatetool.editor/src/com/archimatetool/editor/model/compatibility/handlers/DefaultTextAlignmentHandler.java
@@ -15,24 +15,26 @@ import com.archimatetool.editor.model.compatibility.ICompatibilityHandler;
 import com.archimatetool.editor.utils.StringUtils;
 import com.archimatetool.model.IArchimateModel;
 import com.archimatetool.model.IDiagramModelArchimateObject;
+import com.archimatetool.model.IDiagramModelGroup;
+import com.archimatetool.model.IDiagramModelObject;
 import com.archimatetool.model.IGrouping;
 import com.archimatetool.model.ITextAlignment;
 
 
 
 /**
- *  Convert Grouping Text Alignments < 4.4.0
+ *  Convert Text Alignments that had a default of center to left alignment < 4.4.0
  * 
  * @author Phillip Beauvoir
  */
-public class GroupingTextAlignmentHandler implements ICompatibilityHandler {
+public class DefaultTextAlignmentHandler implements ICompatibilityHandler {
     
     @Override
     public void fixCompatibility(Resource resource) throws CompatibilityHandlerException {
         IArchimateModel model = (IArchimateModel)resource.getContents().get(0);
         
         if(isVersion(model)) {
-            convertGroupingTextPosition(model);
+            convertTextPosition(model);
         }
     }
     
@@ -41,14 +43,17 @@ public class GroupingTextAlignmentHandler implements ICompatibilityHandler {
         return version != null && StringUtils.compareVersionNumbers(version, "4.4.0") < 0; //$NON-NLS-1$
     }
     
-    private void convertGroupingTextPosition(IArchimateModel model) {
+    private void convertTextPosition(IArchimateModel model) {
         for(Iterator<EObject> iter = model.eAllContents(); iter.hasNext();) {
             EObject eObject = iter.next();
-            if(eObject instanceof IDiagramModelArchimateObject && ((IDiagramModelArchimateObject)eObject).getArchimateConcept() instanceof IGrouping) {
-                int textAlignment = ((IDiagramModelArchimateObject)eObject).getTextAlignment();
-                if(textAlignment == ITextAlignment.TEXT_ALIGNMENT_CENTER) {
-                    ((IDiagramModelArchimateObject)eObject).setTextAlignment(ITextAlignment.TEXT_ALIGNMENT_LEFT);
+            
+            if((eObject instanceof IDiagramModelArchimateObject && ((IDiagramModelArchimateObject)eObject).getArchimateConcept() instanceof IGrouping)
+                    || (eObject instanceof IDiagramModelGroup)) {
+                
+                if(((IDiagramModelObject)eObject).getTextAlignment() == ITextAlignment.TEXT_ALIGNMENT_CENTER) {
+                    ((IDiagramModelObject)eObject).setTextAlignment(ITextAlignment.TEXT_ALIGNMENT_LEFT);
                 }
+                
             }
         }
     }
