ada8066b1432a3934080578388c607b6e26031e9
Phillipus <p.beauvoir@dadabeatnik.com>
2019-04-05 22:57:00 +0100

    [Unit Tests] Export model should write to temp folder

--- tests/org.opengroup.archimate.xmlexchange.tests/src/org/opengroup/archimate/xmlexchange/XMLModelExporterTests.java
+++ tests/org.opengroup.archimate.xmlexchange.tests/src/org/opengroup/archimate/xmlexchange/XMLModelExporterTests.java
@@ -59,7 +59,7 @@ public class XMLModelExporterTests {
         // Organization
         exporter.setSaveOrganisation(true);
         
-        File outputFile = new File(TestSupport.testFolder, "archisurance.xml");
+        File outputFile = new File(TestUtils.createTempFolder("XMLModelExporterTests"), "archisurance.xml");
         exporter.exportModel(model, outputFile);
     }
 
