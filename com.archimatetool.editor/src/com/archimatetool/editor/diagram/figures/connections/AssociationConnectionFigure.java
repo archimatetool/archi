4461bcdc06efe36c4f26bbbfb2b677d570d478c9
Phillipus <p.beauvoir@dadabeatnik.com>
2019-09-22 08:18:14 +0100

    [Archimate 3.1] Add UI for Association relationship isDirected attribute
    * Properties Section
    * Connection Figure
    * Zest figure

--- com.archimatetool.editor/src/com/archimatetool/editor/diagram/figures/connections/AssociationConnectionFigure.java
+++ com.archimatetool.editor/src/com/archimatetool/editor/diagram/figures/connections/AssociationConnectionFigure.java
@@ -5,6 +5,12 @@
  */
 package com.archimatetool.editor.diagram.figures.connections;
 
+import org.eclipse.draw2d.PolylineDecoration;
+import org.eclipse.draw2d.RotatableDecoration;
+import org.eclipse.draw2d.geometry.PointList;
+
+import com.archimatetool.model.IAssociationRelationship;
+
 /**
  * Association Connection Figure
  * 
@@ -12,10 +18,45 @@ package com.archimatetool.editor.diagram.figures.connections;
  */
 public class AssociationConnectionFigure extends AbstractArchimateConnectionFigure {
     
+    private static final PointList POINTS = new PointList();
+
+    static {
+        POINTS.addPoint(0, 0);
+        POINTS.addPoint(-1, -1);
+    }
+    
+    /**
+     * @return Decoration to use on Target Node
+     */
+    public static RotatableDecoration createFigureTargetDecoration() {
+        PolylineDecoration dec = new PolylineDecoration();
+        dec.setTemplate(POINTS);
+        dec.setScale(9, 4);
+        return dec;
+    }
+
+    private RotatableDecoration fDecoratorTarget = createFigureTargetDecoration();
+    
     public AssociationConnectionFigure() {
     }
 	
     @Override
     protected void setFigureProperties() {
+    }
+    
+    @Override
+    public void refreshVisuals() {
+        // Access type
+        IAssociationRelationship relation = (IAssociationRelationship)getModelConnection().getArchimateRelationship();
+        
+        if(relation.isDirected()) {
+            setTargetDecoration(fDecoratorTarget); // half-arrow at target endpoint
+        }
+        else {
+            setTargetDecoration(null);
+        }
+
+        // This last
+        super.refreshVisuals();
     }
 }
