c5bde2c17cdce7b0c6a394777b39e48e14cba282
Phillipus <p.beauvoir@dadabeatnik.com>
2019-12-26 19:54:50 +0000

    Splash Handler Mac fix
    * Yep, it worked on Windows and Linux but not Mac (what a surprise)

--- com.archimatetool.editor/src/com/archimatetool/editor/SplashHandler.java
+++ com.archimatetool.editor/src/com/archimatetool/editor/SplashHandler.java
@@ -5,8 +5,12 @@
  */
 package com.archimatetool.editor;
 
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.splash.AbstractSplashHandler;
 
@@ -23,10 +27,16 @@ public class SplashHandler extends AbstractSplashHandler {
 	@Override
     public void init(Shell shell) {
         super.init(shell);
+        shell.setBackgroundMode(SWT.INHERIT_FORCE);
+        
+        String version = Messages.SplashHandler_0 + System.getProperty(Application.APPLICATION_VERSIONID);
 
-        final String version = Messages.SplashHandler_0 + System.getProperty(Application.APPLICATION_VERSIONID);
-
-        shell.addPaintListener(new PaintListener() {
+        // Have to use a child composite to draw on because...yes, you guessed it, otherwise it doesn't work on Mac
+        Composite parent = new Composite(shell, SWT.NONE);
+        Point size = shell.getSize();
+        parent.setBounds(new Rectangle(0, 0, size.x, size.y));
+        
+        parent.addPaintListener(new PaintListener() {
             @Override
             public void paintControl(PaintEvent e) {
                 e.gc.setForeground(ColorFactory.get(35, 35, 140));
