5bc0a15edb3bf9e9b9c2af59af8431385c1fd6a1
Phillipus <p.beauvoir@dadabeatnik.com>
2020-11-07 17:32:41 +0000

    [Eclipse 4.17] Update to latest Themes

---
+++ com.archimatetool.editor.themes/themes/eclipse/css/common/e4_globalstyle.css
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2020 IBM Corporation and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+ 
+.MTrimmedWindow.topLevel {
+	margin-top: 0px;
+	margin-bottom: 0px;
+	margin-left: 0px;
+	margin-right: 0px;
+}
+
+.MPartStack Tree, .MPartStack Table {
+	font-family: '#org-eclipse-ui-workbench-TREE_TABLE_FONT';
+}
