e2960836623bbdb35f2eb5b32189388d1b2706d5
etienne-sf <etienne_sf@users.sf.net>
2020-12-26 17:10:09 +0100

    ChangeElementTypeActionTests added to the test chain

--- tests/com.archimatetool.editor.tests/src/com/archimatetool/editor/diagram/AllDiagramTests.java
+++ tests/com.archimatetool.editor.tests/src/com/archimatetool/editor/diagram/AllDiagramTests.java
@@ -7,6 +7,7 @@ package com.archimatetool.editor.diagram;
 
 import junit.framework.TestSuite;
 
+import com.archimatetool.editor.diagram.actions.ChangeElementTypeActionTests;
 import com.archimatetool.editor.diagram.actions.CopySnapshotTests;
 import com.archimatetool.editor.diagram.actions.SelectAllActionTests;
 import com.archimatetool.editor.diagram.commands.AllCommandsTests;
@@ -21,42 +22,43 @@ import com.archimatetool.editor.diagram.util.DiagramUtilsTests;
 @SuppressWarnings("nls")
 public class AllDiagramTests {
 
-    public static junit.framework.Test suite() {
+	public static junit.framework.Test suite() {
 		TestSuite suite = new TestSuite("com.archimatetool.editor.diagram");
-		
-        // diagram
+
+		// diagram
 		suite.addTest(ArchimateDiagramModelFactoryTests.suite());
-        suite.addTest(DiagramEditorFindReplaceProviderTests.suite());
-        suite.addTest(ImageExportProviderTests.suite());
-        suite.addTest(ImageExportProviderManagerTests.suite());
+		suite.addTest(DiagramEditorFindReplaceProviderTests.suite());
+		suite.addTest(ImageExportProviderTests.suite());
+		suite.addTest(ImageExportProviderManagerTests.suite());
 
-        // diagram.actions
-        suite.addTest(CopySnapshotTests.suite());
-        suite.addTest(SelectAllActionTests.suite());
-        
-        // diagram.commands
-        suite.addTest(AllCommandsTests.suite());
+		// diagram.actions
+		suite.addTest(ChangeElementTypeActionTests.suite());
+		suite.addTest(CopySnapshotTests.suite());
+		suite.addTest(SelectAllActionTests.suite());
 
-        // diagram.editparts
-        suite.addTest(AllEditPartsTests.suite());
-        
-        // diagram.figures
-        suite.addTest(AllFiguresTests.suite());
+		// diagram.commands
+		suite.addTest(AllCommandsTests.suite());
 
-        // diagram.policies
-        suite.addTest(AllPoliciesTests.suite());
+		// diagram.editparts
+		suite.addTest(AllEditPartsTests.suite());
 
-        // diagram.sketch
-        suite.addTest(AllSketchTests.suite());
+		// diagram.figures
+		suite.addTest(AllFiguresTests.suite());
 
-        // diagram.tools
-        suite.addTest(FormatPainterInfoTests.suite());
+		// diagram.policies
+		suite.addTest(AllPoliciesTests.suite());
+
+		// diagram.sketch
+		suite.addTest(AllSketchTests.suite());
+
+		// diagram.tools
+		suite.addTest(FormatPainterInfoTests.suite());
 		suite.addTest(FormatPainterToolTests.suite());
 
-        // diagram.util
-        suite.addTest(DiagramUtilsTests.suite());
+		// diagram.util
+		suite.addTest(DiagramUtilsTests.suite());
 
-        return suite;
+		return suite;
 	}
 
 }
