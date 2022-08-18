53a2f0468571bca90141c9b5f65d766137b43bcc
Phillipus <p.beauvoir@dadabeatnik.com>
2022-08-18 16:26:10 +0100

    [Tests] Add Eclipse-ExtensibleAPI: true to all bundles that have tests
    - This exposes all classes in test fragments
    - The suite no longer needs to use a classloader
    - Eclipse-ExtensibleAPI:true already exists in com.archimatetool.editor in order to expose testing classes in com.archimatetool.editor.tests
    - SWT is using Eclipse-ExtensibleAPI: true
    - See https://stackoverflow.com/questions/41195127/what-is-the-right-way-to-create-junit-tests-for-eclipse-fragments
    - See http://rcpquickstart.wordpress.com/2007/06/20/unit-testing-plug-ins-with-fragments/

--- tests/com.archimatetool.tests/src/com/archimatetool/tests/AllTests.java
+++ tests/com.archimatetool.tests/src/com/archimatetool/tests/AllTests.java
@@ -5,9 +5,6 @@
  */
 package com.archimatetool.tests;
 
-import java.lang.reflect.Method;
-
-import junit.framework.Test;
 import junit.framework.TestSuite;
 
 @SuppressWarnings("nls")
@@ -16,35 +13,21 @@ public class AllTests {
     public static junit.framework.Test suite() {
 		TestSuite suite = new TestSuite("All JUnit Tests");
 		
-        suite.addTest(getTest("com.archimatetool.canvas.AllTests"));
-        suite.addTest(getTest("com.archimatetool.commandline.AllTests"));
-        suite.addTest(getTest("com.archimatetool.csv.AllTests"));
-        suite.addTest(getTest("com.archimatetool.editor.AllTests"));
-        suite.addTest(getTest("com.archimatetool.export.svg.AllTests"));
-        suite.addTest(getTest("com.archimatetool.hammer.AllTests"));
-        suite.addTest(getTest("com.archimatetool.help.AllTests"));
-        suite.addTest(getTest("com.archimatetool.jasperreports.AllTests"));
-        suite.addTest(getTest("com.archimatetool.jdom.AllTests"));
-		suite.addTest(getTest("com.archimatetool.model.AllTests"));
-        suite.addTest(getTest("com.archimatetool.modelimporter.AllTests"));
-        suite.addTest(getTest("com.archimatetool.reports.AllTests"));
-        suite.addTest(getTest("com.archimatetool.zest.AllTests"));
-        suite.addTest(getTest("org.opengroup.archimate.xmlexchange.AllTests"));
+		suite.addTest(com.archimatetool.canvas.AllTests.suite());
+		suite.addTest(com.archimatetool.commandline.AllTests.suite());
+		suite.addTest(com.archimatetool.csv.AllTests.suite());
+		suite.addTest(com.archimatetool.editor.AllTests.suite());
+		suite.addTest(com.archimatetool.export.svg.AllTests.suite());
+		suite.addTest(com.archimatetool.hammer.AllTests.suite());
+		suite.addTest(com.archimatetool.help.AllTests.suite());
+		suite.addTest(com.archimatetool.jasperreports.AllTests.suite());
+		suite.addTest(com.archimatetool.jdom.AllTests.suite());
+		suite.addTest(com.archimatetool.model.AllTests.suite());
+		suite.addTest(com.archimatetool.modelimporter.AllTests.suite());
+		suite.addTest(com.archimatetool.reports.AllTests.suite());
+		suite.addTest(com.archimatetool.zest.AllTests.suite());
+		suite.addTest(org.opengroup.archimate.xmlexchange.AllTests.suite());
 
 		return suite;
-	}
-
-    /**
-     * Use reflection to get classes in test fragments
-     * See http://rcpquickstart.wordpress.com/2007/06/20/unit-testing-plug-ins-with-fragments/
-     */
-	private static Test getTest(String suiteClassName) {
-		try {
-			Class<?> clazz = Class.forName(suiteClassName);
-			Method suiteMethod = clazz.getMethod("suite", new Class[0]);
-			return (Test) suiteMethod.invoke(null, new Object[0]);
-		} catch (Exception e) {
-			throw new RuntimeException("Error", e);
-		}
 	}
 }
