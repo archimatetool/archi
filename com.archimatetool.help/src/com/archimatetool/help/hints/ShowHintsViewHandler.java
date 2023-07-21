8bf4add473a3ba1bc68dcfe8f1dffb7586f8cd8b
Phillipus <p.beauvoir@dadabeatnik.com>
2023-07-21 12:55:14 +0100

    User different keys for shortcuts
    - See https://github.com/archimatetool/archi/issues/948
    - And remove Windows workaround as it's no longer needed

--- com.archimatetool.help/src/com/archimatetool/help/hints/ShowHintsViewHandler.java
+++ com.archimatetool.help/src/com/archimatetool/help/hints/ShowHintsViewHandler.java
@@ -8,12 +8,8 @@ package com.archimatetool.help.hints;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Event;
-import org.eclipse.ui.IViewPart;
 
 import com.archimatetool.editor.ui.services.ViewManager;
-import com.archimatetool.editor.utils.PlatformUtils;
 
 
 
@@ -26,31 +22,7 @@ public class ShowHintsViewHandler extends AbstractHandler {
 
     @Override
     public Object execute(ExecutionEvent event) throws ExecutionException {
-        
-        // Eclipse Bug on Windows when using Alt-7 key command to close a View containing a Browser - it crashes!
-        
-        // Key code of non-zero indicates a key command
-        if(PlatformUtils.isWindows() && event.getTrigger() instanceof Event && ((Event)event.getTrigger()).keyCode != 0) {
-            IViewPart part = ViewManager.findViewPart(IHintsView.ID);
-            
-            // We are closing the View so put it on a thread for the workaround
-            if(part != null) {
-                Display.getCurrent().asyncExec(new Runnable() {
-                    @Override
-                    public void run() {
-                        ViewManager.hideViewPart(IHintsView.ID);
-                    }
-                });
-            }
-            // We are opening the View
-            else {
-                ViewManager.showViewPart(IHintsView.ID, true);
-            }
-        }
-        else {
-            ViewManager.toggleViewPart(IHintsView.ID, true);
-        }
-        
+        ViewManager.toggleViewPart(IHintsView.ID, true);
         return null;
     }
 
