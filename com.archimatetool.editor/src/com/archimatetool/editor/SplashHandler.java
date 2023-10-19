f247a7eb13763972488babc4fdaf2a1aa6d185ee
Phillipus <p.beauvoir@dadabeatnik.com>
2023-10-19 10:49:13 +0100

    Workaround splash image inverted on Mac Sonoma

--- com.archimatetool.editor/src/com/archimatetool/editor/SplashHandler.java
+++ com.archimatetool.editor/src/com/archimatetool/editor/SplashHandler.java
@@ -6,14 +6,18 @@
 package com.archimatetool.editor;
 
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.PaintEvent;
-import org.eclipse.swt.events.PaintListener;
 import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.graphics.Transform;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.splash.AbstractSplashHandler;
+
+import com.archimatetool.editor.utils.PlatformUtils;
 
 /**
  * Splash Handler
@@ -26,6 +30,10 @@ public class SplashHandler extends AbstractSplashHandler {
 	@Override
     public void init(Shell shell) {
         super.init(shell);
+
+        // Check for Mac bug
+        checkMacBackgroundImageInverted(shell);
+        
         shell.setBackgroundMode(SWT.INHERIT_FORCE);
         
         String version = Messages.SplashHandler_0 + " " + ArchiPlugin.INSTANCE.getVersion(); //$NON-NLS-1$
@@ -35,12 +43,53 @@ public class SplashHandler extends AbstractSplashHandler {
         Point size = shell.getSize();
         parent.setBounds(new Rectangle(0, 0, size.x, size.y));
         
-        parent.addPaintListener(new PaintListener() {
-            @Override
-            public void paintControl(PaintEvent e) {
-                e.gc.setForeground(new Color(35, 35, 140));
-                e.gc.drawString(version, 19, 166, true);
-            }
+        parent.addPaintListener(e -> {
+            e.gc.setForeground(new Color(35, 35, 140));
+            e.gc.drawString(version, 19, 166, true);
         });
 	}
+	
+	/**
+	 * macOS Sonoma 14 flips background images on Control#setBackgroundImage
+	 * See https://github.com/eclipse-platform/eclipse.platform.swt/issues/772
+	 */
+	private void checkMacBackgroundImageInverted(Shell shell) {
+        if(PlatformUtils.isMac() && PlatformUtils.compareOSVersion("14.0") >= 0) { //$NON-NLS-1$
+	        // Flip the image
+            shell.setBackgroundImage(flipImage(shell.getDisplay(), shell.getBackgroundImage()));
+            
+            // We are now responsible for disposing the new image
+            shell.addDisposeListener(e -> {
+                shell.getBackgroundImage().dispose();
+            });
+        }
+	}
+	
+    /**
+     * From https://github.com/knime/knime-product/commit/095c88e22ba6d84e22a480f50734141836098169
+     */
+    private Image flipImage(Display display, Image srcImage) {
+        Rectangle bounds = srcImage.getBounds();
+        Image newImage = new Image(display, bounds.width, bounds.height);
+        
+        GC gc = new GC(newImage);
+        gc.setAdvanced(true);
+        gc.setAntialias(SWT.ON);
+        gc.setInterpolation(SWT.HIGH);
+        
+        Transform transform = new Transform(display);
+        // flip down
+        transform.setElements(1, 0, 0, -1, 0, 0);
+        // move up
+        transform.translate(0, -bounds.height);
+        gc.setTransform(transform);
+        
+        gc.drawImage(srcImage, 0, 0, bounds.width, bounds.height,
+                               0, 0, bounds.width, bounds.height);
+        
+        gc.dispose();
+        transform.dispose();
+        
+        return newImage;
+    }
 }
