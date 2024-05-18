8c6906fa5aabfd67979858f42500c8ee487aff6b
Phillipus <p.beauvoir@dadabeatnik.com>
2024-05-18 18:25:41 +0100

    [GEF] Use latest GEF code for SharedCursors
    - Use latest SharedCursors from GEF
    - Load PNGs for cursors
    - Fixes failure to load on Linux - see https://github.com/archimatetool/archi/issues/1036#issuecomment-2118878984

--- org.eclipse.gef/src/org/eclipse/gef/SharedCursors.java
+++ org.eclipse.gef/src/org/eclipse/gef/SharedCursors.java
@@ -1,9 +1,11 @@
 /*******************************************************************************
  * Copyright (c) 2000, 2010 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
@@ -13,14 +15,14 @@ package org.eclipse.gef;
 import org.eclipse.swt.graphics.Cursor;
 
 import org.eclipse.jface.resource.ImageDescriptor;
-
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.draw2d.Cursors;
 
 import org.eclipse.gef.internal.Internal;
 
 /**
  * A shared collection of Cursors.
- * 
+ *
  * @since 2.0
  */
 public class SharedCursors extends Cursors {
@@ -42,24 +44,36 @@ public class SharedCursors extends Cursors {
      */
     public static final Cursor CURSOR_TREE_MOVE;
 
+    private static int deviceZoom = -1;
+
     static {
-        CURSOR_PLUG = createCursor("icons/plugmask.gif", //$NON-NLS-1$
-                "icons/plug.bmp"); //$NON-NLS-1$
-        CURSOR_PLUG_NOT = createCursor("icons/plugmasknot.gif", //$NON-NLS-1$
-                "icons/plugnot.bmp"); //$NON-NLS-1$
-        CURSOR_TREE_ADD = createCursor("icons/Tree_Add_Mask.gif", //$NON-NLS-1$
-                "icons/Tree_Add.gif"); //$NON-NLS-1$
-        CURSOR_TREE_MOVE = createCursor("icons/Tree_Move_Mask.gif", //$NON-NLS-1$
-                "icons/Tree_Move.gif"); //$NON-NLS-1$
+        CURSOR_PLUG = createCursor("icons/plug-cursor.png"); //$NON-NLS-1$
+        CURSOR_PLUG_NOT = createCursor("icons/plugnot-cursor.png"); //$NON-NLS-1$
+        CURSOR_TREE_ADD = createCursor("icons/tree_add-cursor.png"); //$NON-NLS-1$
+        CURSOR_TREE_MOVE = createCursor("icons/tree_move-cursor.png"); //$NON-NLS-1$
     }
 
-    @SuppressWarnings("deprecation")
-    private static Cursor createCursor(String sourceName, String maskName) {
-        ImageDescriptor src = ImageDescriptor.createFromFile(Internal.class,
-                sourceName);
-        ImageDescriptor mask = ImageDescriptor.createFromFile(Internal.class,
-                maskName);
-        return new Cursor(null, src.getImageData(), mask.getImageData(), 0, 0);
+    private static Cursor createCursor(String sourceName) {
+        ImageDescriptor src = ImageDescriptor.createFromFile(Internal.class, sourceName);
+        return new Cursor(null, src.getImageData(getDeviceZoom()), 0, 0);
     }
 
+    private static int getDeviceZoom() {
+        if (deviceZoom == -1) {
+            deviceZoom = 100; // default value
+            String deviceZoomProperty = System.getProperty("org.eclipse.swt.internal.deviceZoom"); //$NON-NLS-1$
+            if (deviceZoomProperty != null) {
+                try {
+                    deviceZoom = Integer.parseInt(deviceZoomProperty);
+                } catch (NumberFormatException ex) {
+                    // if the property can not be parsed we keep the default 100% zoom level
+                }
+            }
+        }
+        // On Mac and Linux X11 ImageData for cursors should always be created with 100% device zoom 
+        return Platform.getOS().equals(Platform.OS_MACOSX) ||
+                (Platform.getOS().equals(Platform.OS_LINUX) && "x11".equalsIgnoreCase(System.getenv("XDG_SESSION_TYPE"))) //$NON-NLS-1$ //$NON-NLS-2$
+                ? 100 
+                : deviceZoom;
+    }
 }
