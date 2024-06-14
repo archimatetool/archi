archimatetool/archi

commit 9ed9333cc6f53871b972937c7e69bf5793827b0f

Show key binding mnemonic for main menu Preferences action

com.archimatetool.editor/src/com/archimatetool/editor/actions/ArchiActionBarAdvisor.java
@@ -356,13 +356,15 @@ extends ActionBarAdvisor {
         menu.add(new GroupMarker(IWorkbenchActionConstants.MB_ADDITIONS));
         
         /*
-         * On a Mac, there is a "Preferences" menu item under the application menu bar.
+         * On a Mac, there is a "Preferences" menu item under the application menu bar
+         * So we only create this one on Windows and Linux.
          */
         if(!PlatformUtils.isMac()) {
-            IWorkbenchAction preferenceAction = ActionFactory.PREFERENCES.create(window);
-            ActionContributionItem item = new ActionContributionItem(preferenceAction);
             menu.add(new Separator());
-            menu.add(item);
+            IWorkbenchAction preferenceAction = ActionFactory.PREFERENCES.create(window);
+            preferenceAction.setActionDefinitionId(IWorkbenchCommandConstants.WINDOW_PREFERENCES); // Support key binding
+            register(preferenceAction);
+            menu.add(preferenceAction);
         }
 
         menu.add(new Separator(IWorkbenchActionConstants.EDIT_END));

commit 0dab84a85e3ed0ed38a4a7fff7c01d8e53c58ff5

Ensure Preference Page key bindings work on Windows and Linux part 2
- This has nothing to do with Eclipse 4.27 as mentioned in the last commit. It is caused by 9ed9333c
- Don't call preferenceAction.setActionDefinitionId()
- The key binding will work, it just won't show as a mnemonic in the menu
- This is how Eclipse itself does it

com.archimatetool.editor/src/com/archimatetool/editor/actions/ArchiActionBarAdvisor.java
@@ -357,19 +357,19 @@ extends ActionBarAdvisor {
         
         /*
          * On a Mac, there is a "Preferences" menu item under the application menu bar
-         * So we only create this one on Windows and Linux.
+         * so we only create this one on Windows and Linux.
          */
         if(!PlatformUtils.isMac()) {
             menu.add(new Separator());
             IWorkbenchAction preferenceAction = ActionFactory.PREFERENCES.create(window);
-            preferenceAction.setActionDefinitionId(IWorkbenchCommandConstants.WINDOW_PREFERENCES); // Support key binding
             /*
-             * Since around Eclipse 4.27 assigning a key shortcut to open a particular preference page stopped working.
-             * Removing the following line seems to fix this.
-             * I'm not sure whether we need to register some actions since their key bindings work anyway.
-             * The New, Open, Save, Find and Select All actions do need to be registered for key bindings to work.
+             * If we want to show the key binding for this action in the menu by calling setActionDefinitionId on it there is a side effect
+             * that opening a particular preference page with a key binding doesn't work (the Preferences dialog will open, but not on that page).
+             * Either don't call setActionDefinitionId or don't register the action here.
+             * Note that Eclipse itself doesn't call setActionDefinitionId on it.
              */
-            //register(preferenceAction);
+            //preferenceAction.setActionDefinitionId(IWorkbenchCommandConstants.WINDOW_PREFERENCES); // Don't do this!
+            register(preferenceAction);
             menu.add(preferenceAction);
         }
 