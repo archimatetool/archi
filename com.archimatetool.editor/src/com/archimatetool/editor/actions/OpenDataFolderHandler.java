07cca793542cb55136539defc963418cca63e6ae
Phillipus <p.beauvoir@dadabeatnik.com>
2024-11-19 08:38:09 +0000

    Use Program.launch to open data folder

--- com.archimatetool.editor/src/com/archimatetool/editor/actions/OpenDataFolderHandler.java
+++ com.archimatetool.editor/src/com/archimatetool/editor/actions/OpenDataFolderHandler.java
@@ -5,14 +5,12 @@
  */
 package com.archimatetool.editor.actions;
 
-import java.awt.Desktop;
-import java.awt.Desktop.Action;
 import java.io.File;
-import java.io.IOException;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.swt.program.Program;
 
 import com.archimatetool.editor.ArchiPlugin;
 
@@ -28,12 +26,7 @@ public class OpenDataFolderHandler extends AbstractHandler {
     @Override
     public Object execute(ExecutionEvent event) throws ExecutionException {
         if(isEnabled()) {
-            try {
-                Desktop.getDesktop().open(ArchiPlugin.INSTANCE.getUserDataFolder());
-            }
-            catch(IOException ex) {
-                ex.printStackTrace();
-            }
+            Program.launch(ArchiPlugin.INSTANCE.getUserDataFolder().getAbsolutePath());
         }
         
         return null;
@@ -42,7 +35,6 @@ public class OpenDataFolderHandler extends AbstractHandler {
     @Override
     public boolean isEnabled() {
         File folder = ArchiPlugin.INSTANCE.getUserDataFolder();
-        // Check AWT Desktop is supported. Some Linux Wayland versions don't support it or need X11. 
-        return Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Action.OPEN) && folder != null && folder.exists();
+        return folder != null && folder.exists();
     }
 }
