0645fe82e79066e20a21abb40ea1cb7edd9f8f05
Phillipus <p.beauvoir@dadabeatnik.com>
2025-02-27 21:57:28 +0000

    NLS - Don't use Messages.class.getPackageName()
    - There's a bug in Eclipse using this

--- com.archimatetool.editor/src/com/archimatetool/editor/ui/textrender/Messages.java
+++ com.archimatetool.editor/src/com/archimatetool/editor/ui/textrender/Messages.java
@@ -4,7 +4,7 @@ import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
 
-    private static final String BUNDLE_NAME = Messages.class.getPackageName() + ".messages"; //$NON-NLS-1$
+    private static final String BUNDLE_NAME = "com.archimatetool.editor.ui.textrender.messages"; //$NON-NLS-1$
 
     public static String RelationshipRenderer_0;
 
