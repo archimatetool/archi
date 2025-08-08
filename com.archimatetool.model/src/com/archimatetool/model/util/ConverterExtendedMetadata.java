414a4f00f0c3e4d282b5a4f575e6f4b6d8e3b250
Phillipus <p.beauvoir@dadabeatnik.com>
2025-08-08 19:44:18 +0100

    Use Hashmap for converter types

--- com.archimatetool.model/src/com/archimatetool/model/util/ConverterExtendedMetadata.java
+++ com.archimatetool.model/src/com/archimatetool/model/util/ConverterExtendedMetadata.java
@@ -5,6 +5,8 @@
  */
 package com.archimatetool.model.util;
 
+import java.util.Map;
+
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EClassifier;
 import org.eclipse.emf.ecore.EPackage;
@@ -19,53 +21,40 @@ import com.archimatetool.model.IArchimatePackage;
  * 
  * @author Phillip Beauvoir
  */
+@SuppressWarnings("nls")
 public class ConverterExtendedMetadata extends BasicExtendedMetaData {
-    /*
-     * Backwards compatibility for previous versions
-     */
+    
+    private static final Map<String, EClassifier> TYPE_MAP = Map.ofEntries(
+        // ArchiMate 2.x
+        Map.entry("UsedByRelationship", IArchimatePackage.Literals.SERVING_RELATIONSHIP),
+        Map.entry("CommunicationPath", IArchimatePackage.Literals.PATH),
+        Map.entry("Network", IArchimatePackage.Literals.COMMUNICATION_NETWORK),
+        Map.entry("InfrastructureInterface", IArchimatePackage.Literals.TECHNOLOGY_INTERFACE),
+        Map.entry("InfrastructureFunction", IArchimatePackage.Literals.TECHNOLOGY_FUNCTION),
+        Map.entry("InfrastructureService", IArchimatePackage.Literals.TECHNOLOGY_SERVICE),
+
+        // Use "z" instead of "s"
+        Map.entry("RealisationRelationship", IArchimatePackage.Literals.REALIZATION_RELATIONSHIP),
+        Map.entry("SpecialisationRelationship", IArchimatePackage.Literals.SPECIALIZATION_RELATIONSHIP),
+
+        // From very old models
+        Map.entry("DiagramModel", IArchimatePackage.Literals.ARCHIMATE_DIAGRAM_MODEL),
+
+        // Since Archi 4.0 we have one Junction type with And/Or attribute
+        Map.entry("AndJunction", IArchimatePackage.Literals.JUNCTION),
+        Map.entry("OrJunction", IArchimatePackage.Literals.JUNCTION),
+
+        // Bogus concept removed in Archi 4.0
+        Map.entry("BusinessActivity", IArchimatePackage.Literals.BUSINESS_PROCESS)
+    );
+    
     @Override
     public EClassifier getType(EPackage ePackage, String name) {
         if(ePackage == IArchimatePackage.eINSTANCE) {
-            switch(name) {
-                // From very old models
-                case "DiagramModel": //$NON-NLS-1$
-                    return IArchimatePackage.Literals.ARCHIMATE_DIAGRAM_MODEL;
-
-                case "UsedByRelationship": //$NON-NLS-1$
-                    return IArchimatePackage.Literals.SERVING_RELATIONSHIP;
-
-                case "RealisationRelationship": //$NON-NLS-1$
-                    return IArchimatePackage.Literals.REALIZATION_RELATIONSHIP;
-
-                case "SpecialisationRelationship": //$NON-NLS-1$
-                    return IArchimatePackage.Literals.SPECIALIZATION_RELATIONSHIP;
-
-                case "AndJunction": //$NON-NLS-1$
-                case "OrJunction": //$NON-NLS-1$
-                    return IArchimatePackage.Literals.JUNCTION;
-
-                case "CommunicationPath": //$NON-NLS-1$
-                    return IArchimatePackage.Literals.PATH;
-
-                case "Network": //$NON-NLS-1$
-                    return IArchimatePackage.Literals.COMMUNICATION_NETWORK;
-
-                case "InfrastructureInterface" : //$NON-NLS-1$
-                    return IArchimatePackage.Literals.TECHNOLOGY_INTERFACE;
-                    
-                case "InfrastructureFunction" : //$NON-NLS-1$
-                    return IArchimatePackage.Literals.TECHNOLOGY_FUNCTION;
-                    
-                case "InfrastructureService" : //$NON-NLS-1$
-                    return IArchimatePackage.Literals.TECHNOLOGY_SERVICE;
-                
-                // Bogus concept removed in Archi 4.0
-                case "BusinessActivity" : //$NON-NLS-1$
-                    return IArchimatePackage.Literals.BUSINESS_PROCESS;
-                    
-                default:
-                    break;
-            } 
+            EClassifier eClassifier = TYPE_MAP.get(name);
+            if(eClassifier != null) {
+                return eClassifier;
+            }
         }
 
         return super.getType(ePackage, name);
@@ -73,7 +62,8 @@ public class ConverterExtendedMetadata extends BasicExtendedMetaData {
     
     @Override
     public EStructuralFeature getAttribute(EClass eClass, String namespace, String name) {
-        if("relationship".equals(name)) { //$NON-NLS-1$
+        // This was used as a relationship reference in a DiagramModelArchimateConnection
+        if("relationship".equals(name) && eClass == IArchimatePackage.eINSTANCE.getDiagramModelArchimateConnection()) {
             return IArchimatePackage.Literals.DIAGRAM_MODEL_ARCHIMATE_CONNECTION__ARCHIMATE_RELATIONSHIP;
         }
         
