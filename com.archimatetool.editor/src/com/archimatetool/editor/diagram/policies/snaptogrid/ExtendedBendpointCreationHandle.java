436384c49b0e3750479cce6ba11f6738ff5ae203
Phillipus <p.beauvoir@dadabeatnik.com>
2025-09-20 16:01:00 +0100

    Improve bendpoint handles
    - Move handles are solid and round and bigger than creation handles
    
    - Creation handles are hollow and round and smaller than move handles
    
    - The primary selection colour is black, non-primary selection colour is grey

--- com.archimatetool.editor/src/com/archimatetool/editor/diagram/policies/snaptogrid/ExtendedBendpointCreationHandle.java
+++ com.archimatetool.editor/src/com/archimatetool/editor/diagram/policies/snaptogrid/ExtendedBendpointCreationHandle.java
@@ -1,50 +1,52 @@
+/**
+ * This program and the accompanying materials
+ * are made available under the terms of the License
+ * which accompanies this distribution in the file LICENSE.txt
+ */
 package com.archimatetool.editor.diagram.policies.snaptogrid;
 
+import org.eclipse.draw2d.ColorConstants;
+import org.eclipse.draw2d.Graphics;
+import org.eclipse.draw2d.MidpointLocator;
 import org.eclipse.draw2d.geometry.Dimension;
-
-/**
- * snap-to-grid patch by Jean-Baptiste Sarrodie (aka Jaiguru)
- * 
- * This class has been extended to allow overriding
- * of class ExtendedConnectionBendpointTracker
- */
-
-/*******************************************************************************
- * Copyright (c) 2000, 2010 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *     IBM Corporation - initial API and implementation
- *******************************************************************************/
-//package org.eclipse.gef.handles;
-
+import org.eclipse.draw2d.geometry.Rectangle;
 import org.eclipse.gef.ConnectionEditPart;
 import org.eclipse.gef.DragTracker;
 import org.eclipse.gef.RequestConstants;
 import org.eclipse.gef.handles.BendpointCreationHandle;
-// snap-to-grid patch
-// Use alternate ConnectionBendpointTracker
-//import org.eclipse.gef.tools.ConnectionBendpointTracker;
+import org.eclipse.swt.graphics.Color;
 
 /**
- * A BendpointHandle that is used to create a new bendpoint.
+ * snap-to-grid patch by Jean-Baptiste Sarrodie (aka Jaiguru)
+ * 
+ * This class has been extended to allow overriding of class ExtendedConnectionBendpointTracker
+ * 
+ * September 2025 - added custom colouring and round graphics
  */
 public class ExtendedBendpointCreationHandle extends BendpointCreationHandle {
 
+    /**
+     * Creates a new ExtendedBendpointCreationHandle, sets its owner to
+     * <code>owner</code> and its index to <code>index</code>, and sets its
+     * locator to a new {@link MidpointLocator} with the given
+     * <code>locatorIndex</code>.
+     * 
+     * @param owner
+     *            the ConnectionEditPart owner
+     * @param index
+     *            the index
+     * @param locatorIndex
+     *            the locator index
+     */
 	public ExtendedBendpointCreationHandle(ConnectionEditPart owner, int index, int locatorIndex) {
 	    super(owner, index, locatorIndex);
-	    
-	    // Make the handle one pixel bigger
-	    setPreferredSize(new Dimension(DEFAULT_HANDLE_SIZE - 1, DEFAULT_HANDLE_SIZE - 1));
+	    setPreferredSize(new Dimension(6, 6));
     }
 
     /**
 	 * Creates and returns a new {@link ExtendedConnectionBendpointTracker}.
 	 * 
-	 * @return the new ConnectionBendpointTracker
+	 * @return the new ExtendedConnectionBendpointTracker
 	 */
 	@Override
     protected DragTracker createDragTracker() {
@@ -53,6 +55,40 @@ public class ExtendedBendpointCreationHandle extends BendpointCreationHandle {
 		tracker.setDefaultCursor(getCursor());
 		return tracker;
 	}
+	
+    /**
+     * Over-ride for custom colors
+     */
+    @Override
+    protected Color getBorderColor() {
+        return isPrimary() ? ColorConstants.black : ColorConstants.gray;
+    }
+    
+    /**
+     * Over-ride for custom colors
+     */
+    @Override
+    protected Color getFillColor() {
+        return ColorConstants.white;
+    }
 
+    /**
+     * Over-ride to draw a circle instead of a square
+     */
+    @Override
+    public void paintFigure(Graphics g) {
+        Rectangle r = getBounds();
+        r.shrink(1, 1);
+        try {
+            g.setBackgroundColor(getFillColor());
+            g.fillOval(r);
+            g.setForegroundColor(getBorderColor());
+            g.drawOval(r);
+        }
+        finally {
+            // We don't really own rect 'r', so fix it.
+            r.expand(1, 1);
+        }
+    }
 }
 
--- com.archimatetool.editor/src/com/archimatetool/editor/diagram/policies/snaptogrid/ExtendedBendpointMoveHandle.java
+++ com.archimatetool.editor/src/com/archimatetool/editor/diagram/policies/snaptogrid/ExtendedBendpointMoveHandle.java
@@ -1,61 +1,35 @@
+/**
+ * This program and the accompanying materials
+ * are made available under the terms of the License
+ * which accompanies this distribution in the file LICENSE.txt
+ */
 package com.archimatetool.editor.diagram.policies.snaptogrid;
 
-/**
- * snap-to-grid patch by Jean-Baptiste Sarrodie (aka Jaiguru)
- * 
- * This class has been extended to allow overriding
- * of class ExtendedConnectionBendpointTracker
- */
-
-/*******************************************************************************
- * Copyright (c) 2000, 2010 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *     IBM Corporation - initial API and implementation
- *******************************************************************************/
-//package org.eclipse.gef.handles;
-
 import org.eclipse.draw2d.BendpointLocator;
-import org.eclipse.draw2d.Locator;
+import org.eclipse.draw2d.ColorConstants;
+import org.eclipse.draw2d.Graphics;
+import org.eclipse.draw2d.geometry.Dimension;
+import org.eclipse.draw2d.geometry.Rectangle;
 import org.eclipse.gef.ConnectionEditPart;
 import org.eclipse.gef.DragTracker;
 import org.eclipse.gef.RequestConstants;
 import org.eclipse.gef.handles.BendpointMoveHandle;
-// snap-to-grid patch
-// Use alternate ConnectionBendpointTracker 
-//import org.eclipse.gef.tools.ConnectionBendpointTracker;
+import org.eclipse.swt.graphics.Color;
 
 /**
- * A BendpointHandle that is used to move an existing bendpoint.
+ * snap-to-grid patch by Jean-Baptiste Sarrodie (aka Jaiguru)
+ * 
+ * This class has been extended to allow overriding of class ExtendedConnectionBendpointTracker
+ * 
+ * SSeptember 2025 - added custom colouring and round graphics
  */
 public class ExtendedBendpointMoveHandle extends BendpointMoveHandle {
-
+    
+    private Dimension selectedSize = new Dimension(9, 9);
+    private Dimension unSelectedSize = new Dimension(8, 8);
+    
 	/**
-	 * Creates a new BendpointMoveHandle.
-	 */
-	public ExtendedBendpointMoveHandle() {
-	}
-
-	/**
-	 * Creates a new BendpointMoveHandle, sets its owner to <code>owner</code>
-	 * and its index to <code>index</code>, and sets its locator to a new
-	 * {@link BendpointLocator}.
-	 * 
-	 * @param owner
-	 *            the ConnectionEditPart owner
-	 * @param index
-	 *            the index
-	 */
-	public ExtendedBendpointMoveHandle(ConnectionEditPart owner, int index) {
-	    super(owner, index);
-	}
-
-	/**
-	 * Creates a new BendpointMoveHandle, sets its owner to <code>owner</code>
+	 * Creates a new ExtendedBendpointMoveHandle, sets its owner to <code>owner</code>
 	 * and its index to <code>index</code>, and sets its locator to a new
 	 * {@link BendpointLocator} with the given <code>locatorIndex</code>.
 	 * 
@@ -71,25 +45,9 @@ public class ExtendedBendpointMoveHandle extends BendpointMoveHandle {
 	}
 
 	/**
-	 * Creates a new BendpointMoveHandle and sets its owner to
-	 * <code>owner</code>, sets its index to <code>index</code>, and sets its
-	 * locator to <code>locator</code>.
-	 * 
-	 * @param owner
-	 *            the ConnectionEditPart owner
-	 * @param index
-	 *            the index
-	 * @param locator
-	 *            the Locator
-	 */
-	public ExtendedBendpointMoveHandle(ConnectionEditPart owner, int index, Locator locator) {
-	    super(owner, index, locator);
-	}
-
-	/**
 	 * Creates and returns a new {@link ExtendedConnectionBendpointTracker}.
 	 * 
-	 * @return the new ConnectionBendpointTracker
+	 * @return the new ExtendedConnectionBendpointTracker
 	 */
 	@Override
     protected DragTracker createDragTracker() {
@@ -99,5 +57,39 @@ public class ExtendedBendpointMoveHandle extends BendpointMoveHandle {
 		return tracker;
 	}
 
+    /**
+     * Over-ride for custom colors
+     */
+	@Override
+    protected Color getBorderColor() {
+	    return isPrimary() ? ColorConstants.white : ColorConstants.gray;
+    }
+	
+	/**
+	 * Over-ride to return a smaller size when not primary selection
+	 */
+	@Override
+	public Dimension getPreferredSize(int wHint, int hHint) {
+	    return isPrimary() ? selectedSize : unSelectedSize;
+	}
+	
+    /**
+     * Over-ride to draw a circle instead of a square
+     */
+    @Override
+    public void paintFigure(Graphics g) {
+        Rectangle r = getBounds();
+        r.shrink(1, 1);
+        try {
+            g.setBackgroundColor(getFillColor());
+            g.fillOval(r);
+            g.setForegroundColor(getBorderColor());
+            g.drawOval(r);
+        }
+        finally {
+            // We don't really own rect 'r', so fix it.
+            r.expand(1, 1);
+        }
+    }
 }
 
