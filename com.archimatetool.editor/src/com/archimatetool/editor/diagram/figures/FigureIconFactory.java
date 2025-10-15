fad164ddfb95acc196e7fe17b0019249f1698228
Phillipus <p.beauvoir@dadabeatnik.com>
2025-10-15 13:07:52 +0100

    Add getImageDescriptorFromFigureIcon to generate icons as ImageDescriptor

--- com.archimatetool.editor/src/com/archimatetool/editor/diagram/figures/FigureIconFactory.java
+++ com.archimatetool.editor/src/com/archimatetool/editor/diagram/figures/FigureIconFactory.java
@@ -6,9 +6,15 @@
 package com.archimatetool.editor.diagram.figures;
 
 import org.eclipse.draw2d.Graphics;
+import org.eclipse.draw2d.SWTGraphics;
 import org.eclipse.draw2d.geometry.Point;
 import org.eclipse.emf.ecore.EClass;
+import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.swt.widgets.Display;
 
 import com.archimatetool.editor.diagram.figures.connections.AccessConnectionFigure;
 import com.archimatetool.editor.diagram.figures.connections.AggregationConnectionFigure;
@@ -81,7 +87,7 @@ public class FigureIconFactory {
      * TODO: all icons need to be drawn with a common origin point so callers can provide one Point
      * 
      * @param eClass The EClass for the figure
-     * @param graphics The grpahics to draw on
+     * @param graphics The graphics to draw on
      * @param foregroundColor
      * @param backgroundColor
      * @param pt The start point to draw the icon
@@ -262,5 +268,49 @@ public class FigureIconFactory {
             }
         }
     }
+    
+    /**
+     * Create an ImageDescriptor for an EClass icon in the FigureIconFactory
+     * The image data size is 16x16 or 32x32 depending on zoom.
+     * 
+     * @param eClass The EClass for the figure
+     * @param foregroundColor
+     * @param backgroundColor
+     * @param pt The start point to draw the icon
+     */
+    public static ImageDescriptor getImageDescriptorFromFigureIcon(EClass eClass, Color foregroundColor, Color backgroundColor, Point pt) {
+        return new ImageDescriptor() {
+            // Set background to this color so we can make it transparent
+            static final Color transparentColor = new Color(255, 255, 254);
+            
+            @Override
+            public ImageData getImageData(int zoom) {
+                // Blank icon image for background and size
+                Image img = new Image(Display.getDefault(), 16, 16);
+                
+                GC gc = new GC(img);
+                
+                // Set background to this color so we can make it transparent
+                gc.setBackground(transparentColor);
+                gc.fillRectangle(0, 0, 16, 16);
+                
+                SWTGraphics graphics = new SWTGraphics(gc);
+                
+                drawIcon(eClass, graphics, foregroundColor, backgroundColor, pt);
+                
+                graphics.dispose();
+                gc.dispose();
+                
+                ImageData data = img.getImageData(zoom);
 
+                // Set transparent pixel to background color
+                data.transparentPixel = data.palette.getPixel(transparentColor.getRGB());
+                
+                img.dispose();
+                
+                return data;
+            }
+        };
+    }
+    
 }
