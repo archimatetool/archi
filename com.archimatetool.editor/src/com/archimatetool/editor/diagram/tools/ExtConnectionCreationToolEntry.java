ddc8db3e0cb1b6e9b53059addc20a4fb8062e8ee
Phillipus <p.beauvoir@dadabeatnik.com>
2025-12-15 17:00:10 +0000

    [Eclipse 4.38] Update cursor creation to use ImageDataProvider for device zoom support

--- com.archimatetool.editor/src/com/archimatetool/editor/diagram/tools/ExtConnectionCreationToolEntry.java
+++ com.archimatetool.editor/src/com/archimatetool/editor/diagram/tools/ExtConnectionCreationToolEntry.java
@@ -13,7 +13,6 @@ import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.swt.graphics.Cursor;
 
 import com.archimatetool.editor.ui.IArchiImages;
-import com.archimatetool.editor.ui.ImageFactory;
 
 /**
  * Extended ConnectionCreationToolEntry so we can set our own "add" cursors instead of the default "plug" ones which are ugly
@@ -22,17 +21,13 @@ import com.archimatetool.editor.ui.ImageFactory;
  */
 public class ExtConnectionCreationToolEntry extends ConnectionCreationToolEntry {
     
-    private static Cursor cursorAdd = new Cursor(
-            null,
-            IArchiImages.ImageFactory.getImage(IArchiImages.CURSOR_IMG_ADD_CONNECTION).getImageData(ImageFactory.getCursorDeviceZoom()),
-            0,
-            0);
+    private static Cursor cursorAdd = new Cursor(null,
+            zoom -> IArchiImages.ImageFactory.getImageDescriptor(IArchiImages.CURSOR_IMG_ADD_CONNECTION).getImageData(zoom),
+            0, 0);
 
-    private static Cursor cursorAddNot = new Cursor(
-            null,
-            IArchiImages.ImageFactory.getImage(IArchiImages.CURSOR_IMG_ADD_NOT_CONNECTION).getImageData(ImageFactory.getCursorDeviceZoom()),
-            0,
-            0);
+    private static Cursor cursorAddNot = new Cursor(null,
+            zoom -> IArchiImages.ImageFactory.getImageDescriptor(IArchiImages.CURSOR_IMG_ADD_NOT_CONNECTION).getImageData(zoom),
+            0, 0);
     
     public ExtConnectionCreationToolEntry(String label, String shortDesc, CreationFactory factory, ImageDescriptor iconSmall, ImageDescriptor iconLarge) {
         super(label, shortDesc, factory, iconSmall, iconLarge);
