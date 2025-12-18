f3fbac83e6a01dad5958b42f2cb53629f2bf5bd6
Phillipus <p.beauvoir@dadabeatnik.com>
2025-12-18 15:22:36 +0000

    Fix DiagramImage and Junction UI providers not exposing User Properties
    - This was a regression introduced in 5a9acbe6787d6f4c80b4fd6994d7b7d4b968b2db

--- com.archimatetool.editor/src/com/archimatetool/editor/ui/factory/elements/JunctionUIProvider.java
+++ com.archimatetool.editor/src/com/archimatetool/editor/ui/factory/elements/JunctionUIProvider.java
@@ -62,13 +62,13 @@ public class JunctionUIProvider extends AbstractArchimateElementUIProvider {
     public Color getDefaultColor() {
         return ColorConstants.black;
     }
-     
+    
     @Override
     public boolean shouldExposeFeature(String featureName) {
         // Junctions don't expose all UI features
-        return /*featureName == IArchimatePackage.Literals.LINE_OBJECT__LINE_COLOR.getName() || */
-                featureName == IArchimatePackage.Literals.DIAGRAM_MODEL_OBJECT__FILL_COLOR.getName() ||
-                        featureName == IArchimatePackage.Literals.DIAGRAM_MODEL_OBJECT__ALPHA.getName(); 
+        return featureName == IArchimatePackage.Literals.PROPERTIES__PROPERTIES.getName() ||
+               featureName == IArchimatePackage.Literals.DIAGRAM_MODEL_OBJECT__FILL_COLOR.getName() ||
+               featureName == IArchimatePackage.Literals.DIAGRAM_MODEL_OBJECT__ALPHA.getName(); 
     }
     
     @Override
