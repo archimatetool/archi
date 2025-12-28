0b5e977fcde5693742c5a411d097d968d9ba57c9
Phillipus <p.beauvoir@dadabeatnik.com>
2025-12-28 22:15:25 +0000

    Update Map View Cheat Sheet
    - Figures should have defaults set for gradients and colors
    - Start a new column every 100 views
    - Requires at least one View
    - Improve rubric

--- com.archimatetool.help/src/com/archimatetool/help/cheatsheets/CreateMapViewCheatSheetAction.java
+++ com.archimatetool.help/src/com/archimatetool/help/cheatsheets/CreateMapViewCheatSheetAction.java
@@ -5,7 +5,8 @@
  */
 package com.archimatetool.help.cheatsheets;
 
-import org.eclipse.emf.common.util.EList;
+import java.util.List;
+
 import org.eclipse.gef.commands.Command;
 import org.eclipse.gef.commands.CommandStack;
 import org.eclipse.jface.action.Action;
@@ -15,6 +16,9 @@ import org.eclipse.ui.IViewPart;
 import org.eclipse.ui.cheatsheets.ICheatSheetAction;
 import org.eclipse.ui.cheatsheets.ICheatSheetManager;
 
+import com.archimatetool.editor.ArchiPlugin;
+import com.archimatetool.editor.preferences.IPreferenceConstants;
+import com.archimatetool.editor.ui.ColorFactory;
 import com.archimatetool.editor.ui.services.EditorManager;
 import com.archimatetool.editor.ui.services.ViewManager;
 import com.archimatetool.editor.views.tree.ITreeModelView;
@@ -23,7 +27,6 @@ import com.archimatetool.model.IArchimateModel;
 import com.archimatetool.model.IDiagramModel;
 import com.archimatetool.model.IDiagramModelReference;
 import com.archimatetool.model.IFolder;
-import com.archimatetool.model.ITextPosition;
 
 
 
@@ -52,8 +55,8 @@ implements ICheatSheetAction {
             return;
         }
         
-        EList<IDiagramModel> diagramModels = model.getDiagramModels();
-        if(diagramModels.size() < 2) {
+        List<IDiagramModel> diagramModels = model.getDiagramModels();
+        if(diagramModels.isEmpty()) {
             MessageDialog.openWarning(Display.getCurrent().getActiveShell(), Messages.CreateMapViewCheatSheetAction_4,
                     Messages.CreateMapViewCheatSheetAction_5);
             return;
@@ -90,35 +93,52 @@ implements ICheatSheetAction {
         
         @Override
         public void redo() {
-            parentFolder.getElements().add(0, diagramModel);
+            parentFolder.getElements().add(diagramModel);
             EditorManager.openDiagramEditor(diagramModel);
         }
         
         private void createMapView(IArchimateModel smodel) {
+            // Create a new DiagramModel
             diagramModel = IArchimateFactory.eINSTANCE.createArchimateDiagramModel();
             diagramModel.setName(Messages.CreateMapViewCheatSheetAction_6);
             
-            // Add diagram model *first* to get id!
-            parentFolder = model.getDefaultFolderForObject(diagramModel);
-            parentFolder.getElements().add(0, diagramModel);
+            // Add all diagram model references...
             
-            // Add diagram model references
-            int y = 20; 
+            final int width = 320;
+            final int height = 100;
+            
+            int count = 0;
+            int xPos = 20;
+            int yPos = 20;
             
             for(IDiagramModel dm : model.getDiagramModels()) {
-                // Don't add the new map view
-                if(dm == diagramModel) {
-                    continue;
-                }
-                
                 IDiagramModelReference ref = IArchimateFactory.eINSTANCE.createDiagramModelReference();
                 ref.setReferencedModel(dm);
-                ref.setBounds(20, y, 400, 100);
-                diagramModel.getChildren().add(ref);
-                y += 120;
+                ref.setBounds(xPos, yPos, width, height);
+                ref.setGradient(ArchiPlugin.getInstance().getPreferenceStore().getInt(IPreferenceConstants.DEFAULT_GRADIENT));
+                ColorFactory.setDefaultColors(ref);
                 
-                ref.setTextPosition(ITextPosition.TEXT_POSITION_TOP);
+                diagramModel.getChildren().add(ref);
+
+                if(++count % 100 == 0) {
+                    xPos += width + 20;
+                    yPos = 20;
+                }
+                else {
+                    yPos += height + 20;
+                }
             }
+            
+            // Add to folder afterwards
+            parentFolder = model.getDefaultFolderForObject(diagramModel);
+            parentFolder.getElements().add(diagramModel);
+        }
+        
+        @Override
+        public void dispose() {
+            model = null;
+            parentFolder = null;
+            diagramModel = null;
         }
     }
     
