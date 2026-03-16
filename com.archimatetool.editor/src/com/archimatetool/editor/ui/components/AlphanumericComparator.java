e544b5f02a50cd27f7f96b68a941bd9ae1db2234
Franky-73 <61655730+Franky-73@users.noreply.github.com>
2026-03-16 23:57:34 +0100

    Improve AlphanumericComparator: deterministic order + hierarchical prefix support

--- com.archimatetool.editor/src/com/archimatetool/editor/ui/components/AlphanumericComparator.java
+++ com.archimatetool.editor/src/com/archimatetool/editor/ui/components/AlphanumericComparator.java
@@ -17,7 +17,7 @@ import java.util.regex.Pattern;
 @SuppressWarnings("nls")
 public class AlphanumericComparator implements Comparator<String> {
     
-    private static final Pattern PATTERN = Pattern.compile("(\\d+)");
+    private static final Pattern HIERARCHY_PATTERN = Pattern.compile("^(\\d+(?:[^0-9]+\\d+)*)([^0-9]+)(.*)$");
 
     private Comparator<? super String> comparator;
 
@@ -27,29 +27,194 @@ public class AlphanumericComparator implements Comparator<String> {
 
     @Override
     public int compare(String s1, String s2) {
-        Matcher m1 = PATTERN.matcher(s1);
-        Matcher m2 = PATTERN.matcher(s2);
-        int pos1 = 0, pos2 = 0;
+        HierarchyLabel h1 = parseHierarchyLabel(s1);
+        HierarchyLabel h2 = parseHierarchyLabel(s2);
 
-        while(m1.find() && m2.find()) {
-            // Compare non-numeric prefixes
-            int cmpPrefix = comparator.compare(s1.substring(pos1, m1.start()), s2.substring(pos2, m2.start()));
-            if(cmpPrefix != 0) {
-                return cmpPrefix;
+        if(h1 != null && h2 != null) {
+            int cmp = compareHierarchyPaths(h1.path, h2.path);
+            if(cmp != 0) {
+                return cmp;
             }
 
-            // Compare numeric parts
-            int num1 = Integer.parseInt(m1.group());
-            int num2 = Integer.parseInt(m2.group());
-            if(num1 != num2) {
-                return Integer.compare(num1, num2);
+            cmp = compareAlphanumericText(h1.remainder, h2.remainder);
+            if(cmp != 0) {
+                return cmp;
             }
 
-            pos1 = m1.end();
-            pos2 = m2.end();
+            return comparator.compare(s1, s2);
         }
-        
-        // Compare remaining parts
-        return comparator.compare(s1.substring(pos1), s2.substring(pos2));
+
+        return compareAlphanumericText(s1, s2);
+    }
+
+    private int compareHierarchyPaths(String path1, String path2) {
+        int i1 = 0;
+        int i2 = 0;
+        int len1 = path1.length();
+        int len2 = path2.length();
+
+        while(i1 < len1 && i2 < len2) {
+            int start1 = i1;
+            int start2 = i2;
+
+            while(i1 < len1 && Character.isDigit(path1.charAt(i1))) {
+                i1++;
+            }
+            while(i2 < len2 && Character.isDigit(path2.charAt(i2))) {
+                i2++;
+            }
+
+            int cmp = compareNumberTokens(path1.substring(start1, i1), path2.substring(start2, i2));
+            if(cmp != 0) {
+                return cmp;
+            }
+
+            int sepStart1 = i1;
+            int sepStart2 = i2;
+
+            while(i1 < len1 && !Character.isDigit(path1.charAt(i1))) {
+                i1++;
+            }
+            while(i2 < len2 && !Character.isDigit(path2.charAt(i2))) {
+                i2++;
+            }
+
+            boolean hasSeparator1 = sepStart1 < i1;
+            boolean hasSeparator2 = sepStart2 < i2;
+
+            boolean hasNextNumber1 = i1 < len1 && Character.isDigit(path1.charAt(i1));
+            boolean hasNextNumber2 = i2 < len2 && Character.isDigit(path2.charAt(i2));
+
+            if(hasSeparator1 && hasNextNumber1 && hasSeparator2 && hasNextNumber2) {
+                continue;
+            }
+
+            if(hasSeparator1 && hasNextNumber1) {
+                return 1;
+            }
+            if(hasSeparator2 && hasNextNumber2) {
+                return -1;
+            }
+
+            break;
+        }
+
+        if(i1 < len1) {
+            return 1;
+        }
+        if(i2 < len2) {
+            return -1;
+        }
+
+        return 0;
+    }
+
+    private int compareAlphanumericText(String s1, String s2) {
+        int i1 = 0;
+        int i2 = 0;
+        int len1 = s1.length();
+        int len2 = s2.length();
+
+        while(i1 < len1 && i2 < len2) {
+            char c1 = s1.charAt(i1);
+            char c2 = s2.charAt(i2);
+
+            if(Character.isDigit(c1) && Character.isDigit(c2)) {
+                int start1 = i1;
+                int start2 = i2;
+
+                while(i1 < len1 && Character.isDigit(s1.charAt(i1))) {
+                    i1++;
+                }
+                while(i2 < len2 && Character.isDigit(s2.charAt(i2))) {
+                    i2++;
+                }
+
+                String token1 = s1.substring(start1, i1);
+                String token2 = s2.substring(start2, i2);
+
+                int cmp = compareNumberTokens(token1, token2);
+                if(cmp != 0) {
+                    return cmp;
+                }
+            }
+            else {
+                int start1 = i1;
+                int start2 = i2;
+
+                while(i1 < len1 && !Character.isDigit(s1.charAt(i1))) {
+                    i1++;
+                }
+                while(i2 < len2 && !Character.isDigit(s2.charAt(i2))) {
+                    i2++;
+                }
+
+                String token1 = s1.substring(start1, i1);
+                String token2 = s2.substring(start2, i2);
+
+                int cmp = comparator.compare(token1, token2);
+                if(cmp != 0) {
+                    return cmp;
+                }
+            }
+        }
+
+        if(i1 < len1) {
+            return 1;
+        }
+        if(i2 < len2) {
+            return -1;
+        }
+
+        return comparator.compare(s1, s2);
+    }
+
+    private int compareNumberTokens(String s1, String s2) {
+        String n1 = stripLeadingZeros(s1);
+        String n2 = stripLeadingZeros(s2);
+
+        if(n1.length() != n2.length()) {
+            return n1.length() - n2.length();
+        }
+
+        int cmp = n1.compareTo(n2);
+        if(cmp != 0) {
+            return cmp;
+        }
+
+        if(s1.length() != s2.length()) {
+            return s1.length() - s2.length();
+        }
+
+        return 0;
+    }
+
+    private String stripLeadingZeros(String s) {
+        int i = 0;
+
+        while(i < s.length() - 1 && s.charAt(i) == '0') {
+            i++;
+        }
+
+        return s.substring(i);
+    }
+
+    private HierarchyLabel parseHierarchyLabel(String s) {
+        Matcher matcher = HIERARCHY_PATTERN.matcher(s);
+        if(!matcher.matches()) {
+            return null;
+        }
+
+        return new HierarchyLabel(matcher.group(1), matcher.group(3));
+    }
+
+    private static final class HierarchyLabel {
+        private final String path;
+        private final String remainder;
+
+        private HierarchyLabel(String path, String remainder) {
+            this.path = path;
+            this.remainder = remainder;
+        }
     }
 }
