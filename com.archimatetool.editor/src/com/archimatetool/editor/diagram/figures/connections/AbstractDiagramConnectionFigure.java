archimatetool/archi

commit 580232635f9d670160d65eb92c090abb7ff2b58b

Refactor ITargetFeedbackFigure and add showTargetFeedback(boolean)
- Add ITargetFeedbackFigure#showTargetFeedback(boolean)
- Deprecate ITargetFeedbackFigure#showTargetFeedback()
- Deprecate ITargetFeedbackFigure#eraseTargetFeedback()
- Refactor and modernise code

com.archimatetool.editor/src/com/archimatetool/editor/diagram/figures/connections/AbstractDiagramConnectionFigure.java
@@ -12,9 +12,6 @@ import org.eclipse.draw2d.ConnectionLocator;
 import org.eclipse.draw2d.Graphics;
 import org.eclipse.draw2d.IFigure;
 import org.eclipse.draw2d.Locator;
-// line-curves patch by Jean-Baptiste Sarrodie (aka Jaiguru)
-// Use alternate PolylineConnection
-//import org.eclipse.draw2d.PolylineConnection;
 import org.eclipse.draw2d.geometry.Point;
 import org.eclipse.draw2d.geometry.Rectangle;
 import org.eclipse.draw2d.text.FlowPage;
@@ -41,39 +38,36 @@ import com.archimatetool.model.IDiagramModelConnection;
  */
 public abstract class AbstractDiagramConnectionFigure
 extends RoundedPolylineConnection implements IDiagramConnectionFigure {
-//extends PolylineConnection implements IDiagramConnectionFigure {
 
-    private TextFlow fTextFlow;
-    
+    private TextFlow textFlow;
+    private IDiagramModelConnection diagramModelConnection;
+
     protected int fTextPosition = -1;
-    
-    private IDiagramModelConnection fDiagramModelConnection;
-
     protected Color fFontColor;
     protected Color fLineColor;
     
-    private static Color TARGET_FEEDBACK_COLOR = new Color(0, 0, 255);
+    protected static Color highlightedColor = new Color(0, 0, 255);
     
-    protected boolean SHOW_TARGET_FEEDBACK = false;
-    
-	@Override
+    protected boolean showTargetFeedback = false;
+
+    @Override
     public void setModelConnection(IDiagramModelConnection connection) {
-	    fDiagramModelConnection = connection;
+	    diagramModelConnection = connection;
 	    
 	    setFigureProperties();
         
         // Have to add this if we want Animation to work!
         AnimationUtil.addConnectionForRoutingAnimation(this);
-	}
-	
-	@Override
+    }
+
+    @Override
     public IDiagramModelConnection getModelConnection() {
-	    return fDiagramModelConnection;
-	}
-	
-	protected void setFigureProperties() {
-	}
-	
+        return diagramModelConnection;
+    }
+
+    protected void setFigureProperties() {
+    }
+
     @Override
     public void refreshVisuals() {
         // If the text position has been changed by user update it
@@ -112,17 +106,17 @@ extends RoundedPolylineConnection implements IDiagramConnectionFigure {
 
     @Override
     public TextFlow getConnectionLabel() {
-        if(fTextFlow == null) {
-            fTextFlow = new TextFlow();
+        if(textFlow == null) {
+            textFlow = new TextFlow();
             //fTextFlow.setLayoutManager(new ParagraphTextLayout(fTextFlow, ParagraphTextLayout.WORD_WRAP_HARD));
             
             FlowPage flowPage = new FlowPage();
-            flowPage.add(fTextFlow);
+            flowPage.add(textFlow);
             
             add(flowPage);
         }
         
-        return fTextFlow;
+        return textFlow;
     }
     
     protected FlowPage getFlowPage() {
@@ -165,14 +159,6 @@ extends RoundedPolylineConnection implements IDiagramConnectionFigure {
         getConnectionLabel().setText(text);
     }
     
-    /**
-     * Deprecated - use setText() instead
-     */
-    @Deprecated
-    protected void setConnectionText() {
-        setText();
-    }
-
     /**
      * Set the font in the label to that in the model, or failing that, as per user's default
      */
@@ -254,32 +240,22 @@ extends RoundedPolylineConnection implements IDiagramConnectionFigure {
     }
     
     @Override
-    public void showTargetFeedback() {
-        if(!SHOW_TARGET_FEEDBACK) {
-            SHOW_TARGET_FEEDBACK = true;
-            repaint();
-        }
+    public void showTargetFeedback(boolean show) {
+        showTargetFeedback = show;
     }
-    
-    @Override
-    public void eraseTargetFeedback() {
-        if(SHOW_TARGET_FEEDBACK) {
-            SHOW_TARGET_FEEDBACK = false;
-            repaint();
-        }
-    }
-    
+
     @Override
     public void paintFigure(Graphics graphics) {
-        if(SHOW_TARGET_FEEDBACK) {
-            setForegroundColor(TARGET_FEEDBACK_COLOR);
+        if(showTargetFeedback) {
             setLineWidth(getModelConnection().getLineWidth() + 1);
+            setForegroundColor(highlightedColor);
         }
         else {
             setLineWidth();
             setForegroundColor(fLineColor);
         }
 
+        // Label strategy
         if(StringUtils.isSet(getConnectionLabel().getText()) && 
                 ArchiPlugin.getInstance().getPreferenceStore().getInt(IPreferenceConstants.CONNECTION_LABEL_STRATEGY) == CONNECTION_LABEL_CLIPPED) {
             clipTextLabel(graphics);

com.archimatetool.editor/src/com/archimatetool/editor/diagram/figures/AbstractContainerFigure.java
@@ -5,7 +5,6 @@
  */
 package com.archimatetool.editor.diagram.figures;
 
-import org.eclipse.draw2d.ColorConstants;
 import org.eclipse.draw2d.FreeformLayer;
 import org.eclipse.draw2d.Graphics;
 import org.eclipse.draw2d.IFigure;
@@ -14,6 +13,7 @@ import org.eclipse.draw2d.geometry.Dimension;
 import org.eclipse.draw2d.geometry.Rectangle;
 import org.eclipse.draw2d.geometry.Translatable;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
 
 import com.archimatetool.editor.diagram.util.AnimationUtil;
 import com.archimatetool.model.IDiagramModelObject;
@@ -27,10 +27,11 @@ import com.archimatetool.model.IDiagramModelObject;
  */
 public abstract class AbstractContainerFigure extends AbstractDiagramModelObjectFigure
 implements IContainerFigure {
-
-    protected boolean SHOW_TARGET_FEEDBACK = false;
     
-    private IFigure fMainFigure;
+    protected static Color highlightedColor = new Color(0, 0, 255);
+
+    protected boolean showTargetFeedback = false;
+    private IFigure mainFigure;
     
     protected AbstractContainerFigure() {
     }
@@ -43,15 +44,15 @@ implements IContainerFigure {
      * @return The main figure to draw on
      */
     public IFigure getMainFigure() {
-        if(fMainFigure == null) {
-            fMainFigure = new FreeformLayer();
-            fMainFigure.setLayoutManager(new XYLayout());
+        if(mainFigure == null) {
+            mainFigure = new FreeformLayer();
+            mainFigure.setLayoutManager(new XYLayout());
             
             // Have to add this if we want Animation to work on figures
-            AnimationUtil.addFigureForAnimation(fMainFigure);
+            AnimationUtil.addFigureForAnimation(mainFigure);
         }
         
-        return fMainFigure;
+        return mainFigure;
     }
     
     @Override
@@ -95,7 +96,7 @@ implements IContainerFigure {
     protected void paintFigure(Graphics graphics) {
         graphics.setAntialias(SWT.ON);
         drawFigure(graphics);
-        if(SHOW_TARGET_FEEDBACK) {
+        if(showTargetFeedback) {
             drawTargetFeedback(graphics);
         }
     }
@@ -125,7 +126,7 @@ implements IContainerFigure {
         }
         
         bounds.shrink(1, 1);
-        graphics.setForegroundColor(ColorConstants.blue);
+        graphics.setForegroundColor(highlightedColor);
         graphics.setLineWidth(2);
         graphics.drawRectangle(bounds);
         
@@ -133,19 +134,10 @@ implements IContainerFigure {
     }
     
     @Override
-    public void eraseTargetFeedback() {
-        if(SHOW_TARGET_FEEDBACK) {
-            SHOW_TARGET_FEEDBACK = false;
+    public void showTargetFeedback(boolean show) {
+        if(showTargetFeedback != show) {
+            showTargetFeedback = show;
             repaint();
         }
     }
-
-    @Override
-    public void showTargetFeedback() {
-        if(!SHOW_TARGET_FEEDBACK) {
-            SHOW_TARGET_FEEDBACK = true;
-            repaint();
-        }
-    }
-    
 }

com.archimatetool.editor/src/com/archimatetool/editor/diagram/figures/ITargetFeedbackFigure.java
@@ -18,11 +18,24 @@ public interface ITargetFeedbackFigure extends IFigure {
     
     /**
      * Show some feedback
+     * @deprecated Use showTargetFeedback(true)
      */
-    void showTargetFeedback();
+    @Deprecated
+    default void showTargetFeedback() {
+    }
 
     /**
      * Erase some feedback
+     * @deprecated Use showTargetFeedback(false)
      */
-    void eraseTargetFeedback();
+    @Deprecated
+    default void eraseTargetFeedback() {
+    }
+    
+    /**
+     * Show target feedback on this figure
+     * @param show
+     */
+    default void showTargetFeedback(boolean show) {
+    }
 }

com.archimatetool.editor/src/com/archimatetool/editor/diagram/policies/ArchimateDiagramConnectionPolicy.java
@@ -7,7 +7,6 @@ package com.archimatetool.editor.diagram.policies;
 
 import java.util.List;
 
-import org.eclipse.draw2d.IFigure;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.gef.GraphicalEditPart;
 import org.eclipse.gef.Request;
@@ -116,8 +115,8 @@ public class ArchimateDiagramConnectionPolicy extends GraphicalNodeEditPolicy {
 
         // Get the type of connection (plain) or relationship (if archimate connection) and check if it is valid
         EClass type = connection.eClass();
-        if(connection instanceof IDiagramModelArchimateConnection) {
-            type = ((IDiagramModelArchimateConnection)connection).getArchimateRelationship().eClass();
+        if(connection instanceof IDiagramModelArchimateConnection archimateConnection) {
+            type = archimateConnection.getArchimateRelationship().eClass();
         }
 
         if(isSourceCommand) {
@@ -132,8 +131,9 @@ public class ArchimateDiagramConnectionPolicy extends GraphicalNodeEditPolicy {
         }
         
         // Archimate type reconnection
-        if(connection instanceof IDiagramModelArchimateConnection && newComponent instanceof IDiagramModelArchimateComponent) {
-            return createArchimateReconnectCommand((IDiagramModelArchimateConnection)connection, (IDiagramModelArchimateComponent)newComponent, isSourceCommand);
+        if(connection instanceof IDiagramModelArchimateConnection archimateConnection
+                && newComponent instanceof IDiagramModelArchimateComponent archimateComponent) {
+            return createArchimateReconnectCommand(archimateConnection, archimateComponent, isSourceCommand);
         }
         
         // Plain reconnection
@@ -250,9 +250,8 @@ public class ArchimateDiagramConnectionPolicy extends GraphicalNodeEditPolicy {
 
     @Override
     public void eraseTargetFeedback(Request request) {
-        IFigure figure = ((GraphicalEditPart)getHost()).getFigure();
-        if(figure instanceof ITargetFeedbackFigure) {
-            ((ITargetFeedbackFigure)figure).eraseTargetFeedback();
+        if(((GraphicalEditPart)getHost()).getFigure() instanceof ITargetFeedbackFigure figure) {
+            figure.showTargetFeedback(false);
         }
     }
     
@@ -284,9 +283,8 @@ public class ArchimateDiagramConnectionPolicy extends GraphicalNodeEditPolicy {
     }
     
     private void showTargetFeedback() {
-        IFigure figure = ((GraphicalEditPart)getHost()).getFigure();
-        if(figure instanceof ITargetFeedbackFigure) {
-            ((ITargetFeedbackFigure)figure).showTargetFeedback();
+        if(((GraphicalEditPart)getHost()).getFigure() instanceof ITargetFeedbackFigure figure) {
+            figure.showTargetFeedback(true);
         }
     }
     
@@ -310,8 +308,7 @@ public class ArchimateDiagramConnectionPolicy extends GraphicalNodeEditPolicy {
         }
         
         // Archimate Concept source
-        if(source instanceof IDiagramModelArchimateComponent) {
-            IDiagramModelArchimateComponent dmc = (IDiagramModelArchimateComponent)source;
+        if(source instanceof IDiagramModelArchimateComponent dmc) {
             return ArchimateModelUtils.isValidRelationshipStart(dmc.getArchimateConcept(), relationshipType);
         }
         
@@ -356,7 +353,7 @@ public class ArchimateDiagramConnectionPolicy extends GraphicalNodeEditPolicy {
         }
 
         // Connection from Archimate concept to Archimate concept (but not from relation to relation)
-        if((source instanceof IDiagramModelArchimateComponent && target instanceof IDiagramModelArchimateComponent) && 
+        if((source instanceof IDiagramModelArchimateComponent archimateSource && target instanceof IDiagramModelArchimateComponent archimateTarget) && 
               !(source instanceof IDiagramModelArchimateConnection && target instanceof IDiagramModelArchimateConnection)) {
             
             // Special case if relationshipType == null. Means that the Magic connector is being used
@@ -364,10 +361,7 @@ public class ArchimateDiagramConnectionPolicy extends GraphicalNodeEditPolicy {
                 return true;
             }
             
-            IArchimateConcept sourceConcept = ((IDiagramModelArchimateComponent)source).getArchimateConcept();
-            IArchimateConcept targetConcept = ((IDiagramModelArchimateComponent)target).getArchimateConcept();
-            
-            return ArchimateModelUtils.isValidRelationship(sourceConcept, targetConcept, relationshipType);
+            return ArchimateModelUtils.isValidRelationship(archimateSource.getArchimateConcept(), archimateTarget.getArchimateConcept(), relationshipType);
         }
         
         return false;

com.archimatetool.editor/src/com/archimatetool/editor/diagram/policies/ContainerHighlightEditPolicy.java
@@ -5,7 +5,6 @@
  */
 package com.archimatetool.editor.diagram.policies;
 
-import org.eclipse.draw2d.IFigure;
 import org.eclipse.gef.EditPart;
 import org.eclipse.gef.GraphicalEditPart;
 import org.eclipse.gef.Request;
@@ -25,13 +24,6 @@ import com.archimatetool.editor.diagram.figures.ITargetFeedbackFigure;
 public class ContainerHighlightEditPolicy extends GraphicalEditPolicy {
 
     @Override
-    public void eraseTargetFeedback(Request request) {
-        if(getFeedbackFigure() != null) {
-            getFeedbackFigure().eraseTargetFeedback();
-        }
-    }
-
-    @Override
     public EditPart getTargetEditPart(Request request) {
         if(request == null || request.getType() == null) {
             return null;
@@ -48,14 +40,16 @@ public class ContainerHighlightEditPolicy extends GraphicalEditPolicy {
                 || request.getType().equals(DiagramDropRequest.REQ_DIAGRAM_DROP)
                 ) {
             
-            if(getFeedbackFigure() != null) {
-                getFeedbackFigure().showTargetFeedback();
+            if(((GraphicalEditPart)getHost()).getFigure() instanceof ITargetFeedbackFigure figure) {
+                figure.showTargetFeedback(true);
             }
         }
     }
-
-    private ITargetFeedbackFigure getFeedbackFigure() {
-        IFigure figure = ((GraphicalEditPart)getHost()).getFigure();
-        return (figure instanceof ITargetFeedbackFigure) ? (ITargetFeedbackFigure)figure : null;
+    
+    @Override
+    public void eraseTargetFeedback(Request request) {
+        if(((GraphicalEditPart)getHost()).getFigure() instanceof ITargetFeedbackFigure figure) {
+            figure.showTargetFeedback(false);
+        }
     }
 }

commit 3c3ffa1325301289b89b441e44df570258e09059

Highlight connection when hovering with connection creation tool or magic connector
The connection should highlight when the cursor hovers over the connection when starting a connection as well as finishing a connection

This fixes a regression introduced in 580232635f9d670160d65eb92c090abb7ff2b58b

com.archimatetool.editor/src/com/archimatetool/editor/diagram/figures/connections/AbstractDiagramConnectionFigure.java
@@ -243,7 +243,10 @@ extends RoundedPolylineConnection implements IDiagramConnectionFigure {
     
     @Override
     public void showTargetFeedback(boolean show) {
-        showTargetFeedback = show;
+        if(showTargetFeedback != show) {
+            showTargetFeedback = show;
+            repaint();
+        }
     }
 
     @Override